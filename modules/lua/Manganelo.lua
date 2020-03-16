756beb36d6b7bd728a58fd24f864700c5e223b64
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2020-03-15 21:46:31 -0600

    Updates for Mangakakalot and Mangabat.com

--- modules/lua/Manganelo.lua
+++ modules/lua/Manganelo.lua
@@ -3,8 +3,10 @@ function Register()
     module.Name = 'Manganelo'
     module.Language = 'English'
 
+    module.Domains.Add('mangabat.com', 'Mangabat.com')
+    module.Domains.Add('mangakakalot.com', 'Mangakakalot')
     module.Domains.Add('manganelo.com', 'Manganelo')
-
+    
 end
 
 function GetInfo()
@@ -14,7 +16,25 @@ function GetInfo()
     info.Author = dom.SelectValues('//td[contains(text(), "Author")]/following-sibling::td/a/text()')
     info.Status = dom.SelectValue('//td[contains(text(), "Status")]/following-sibling::td/text()')
     info.Tags = dom.SelectValues('//td[contains(text(), "Genres")]/following-sibling::td/a/text()')
-    info.Summary = tostring(dom.SelectElement('//div[contains(@class, "info-description")]')):after('Description :')
+    info.Summary = dom.SelectValue('//div[contains(@class, "info-description") or contains(@id, "noidungm")]'):after('Description :')
+
+    -- The following cases apply specifically to Mangakakalot (mangakakalot.com).
+
+    if(isempty(info.AlternativeTitle)) then
+        info.AlternativeTitle = dom.SelectValue('//h2[contains(@class, "alternative")]')
+    end
+
+    if(isempty(info.Author)) then
+        info.Author = dom.SelectValues('//li[contains(text(), "Author")]//a')
+    end
+
+    if(isempty(info.Status)) then
+        info.Status = dom.SelectValue('//li[contains(text(), "Status")]'):after(':')
+    end
+
+    if(isempty(info.Tags)) then
+        info.Tags = dom.SelectValues('//li[contains(text(), "Genres")]//a')
+    end
 
 end
 
@@ -28,6 +48,6 @@ end
 
 function GetPages()
 
-    pages.AddRange(dom.SelectValues('//div[contains(@class, "chapter-reader")]/img/@src'))
+    pages.AddRange(dom.SelectValues('//div[contains(@class, "chapter-reader") or contains(@class, "vung-doc") or contains(@class, "vung_doc")]/img/@src'))
 
 end
