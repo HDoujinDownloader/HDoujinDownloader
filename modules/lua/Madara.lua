0fd42a3672dee367a3ecab9ac6b7b17080998e1f
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2020-03-26 14:32:01 -0600

    Added support for mangawow.com (NON-H)

--- modules/lua/Madara.lua
+++ modules/lua/Madara.lua
@@ -4,25 +4,38 @@
 function Register()
 
     module.Name = 'Madara'
+
+    module = module.New()
+
     module.Language = 'English'
 
     module.Domains.Add('disasterscans.com', 'Disaster Scans')
     module.Domains.Add('porncomixonline.net', 'Porncomix')
     module.Domains.Add('toonily.com', 'Toonily')
 
+    RegisterModule(module)
+
+    module = module.New()
+
+    module.Language = 'Turkish'
+
+    module.Domains.Add('mangawow.com', 'MangaWOW')
+
+    RegisterModule(module)
+
 end
 
 function GetInfo()
 
     info.Title = dom.SelectValue('//h1/text()[last()]')
-    info.AlternativeTitle = dom.SelectValue('//div[contains(h5/text(), "Alternative")]/following-sibling::div')
-    info.Author = dom.SelectValues('//div[contains(h5/text(), "Author(s)") or contains(h5/text(), "Auth.")]/following-sibling::div//a')
-    info.Artist = dom.SelectValues('//div[contains(h5/text(), "Artist(s)") or contains(h5/text(), "Artist")]/following-sibling::div//a')
-    info.Tags = dom.SelectValues('//div[contains(h5/text(), "Genre(s)") or contains(h5/text(), "Genres")]/following-sibling::div//a')
+    info.AlternativeTitle = dom.SelectValue('//div[contains(h5/text(), "Alternative") or contains(h5/text(), "Diğer Adları")]/following-sibling::div')
+    info.Author = dom.SelectValues('//div[contains(h5/text(), "Author(s)") or contains(h5/text(), "Auth.") or contains(h5/text(), "Yazar")]/following-sibling::div//a')
+    info.Artist = dom.SelectValues('//div[contains(h5/text(), "Artist") or contains(h5/text(), "Çizer")]/following-sibling::div//a')
+    info.Tags = dom.SelectValues('//div[contains(h5/text(), "Genre") or contains(h5/text(), "Kategori")]/following-sibling::div//a')
     info.Type = dom.SelectValue('//div[contains(h5/text(), "Type")]/following-sibling::div')
     info.DateReleased = dom.SelectValue('//div[contains(h5/text(), "Release")]/following-sibling::div')
     info.Status = dom.SelectValue('//div[contains(h5/text(), "Status")]/following-sibling::div')
-    info.Summary = dom.SelectValue('//div[contains(@class, "description-summary")]')
+    info.Summary = dom.SelectValue('//div[contains(@class, "description-summary")]//p')
 
     if(module.GetName(url):endswith('Scans')) then
         info.Scanlator = module.GetName(url)
@@ -65,6 +78,12 @@ function GetPages()
     else
 
         pages.AddRange(dom.SelectValues('//div[contains(@class, "reading-content")]//img/@data-src'))
+
+        -- Sometimes the image URLs are in "src" instead of "data-src" (mangawow.com).
+
+        if(pages.Count() <= 0) then
+            pages.AddRange(dom.SelectValues('//div[contains(@class, "reading-content")]//img/@src'))
+        end
 
     end
 
