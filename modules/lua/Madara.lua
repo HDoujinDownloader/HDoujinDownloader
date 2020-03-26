556f001608d400fe1b864331b73bf38946a3de7f
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2020-03-26 14:17:19 -0600

    Updates for porncomixonline.net (updated algorithms)

--- modules/lua/Madara.lua
+++ modules/lua/Madara.lua
@@ -7,6 +7,7 @@ function Register()
     module.Language = 'English'
 
     module.Domains.Add('disasterscans.com', 'Disaster Scans')
+    module.Domains.Add('porncomixonline.net', 'Porncomix')
     module.Domains.Add('toonily.com', 'Toonily')
 
 end
@@ -15,9 +16,9 @@ function GetInfo()
 
     info.Title = dom.SelectValue('//h1/text()[last()]')
     info.AlternativeTitle = dom.SelectValue('//div[contains(h5/text(), "Alternative")]/following-sibling::div')
-    info.Author = dom.SelectValues('//div[contains(h5/text(), "Author(s)")]/following-sibling::div//a')
-    info.Artist = dom.SelectValues('//div[contains(h5/text(), "Artist(s)")]/following-sibling::div//a')
-    info.Tags = dom.SelectValues('//div[contains(h5/text(), "Genre(s)")]/following-sibling::div//a')
+    info.Author = dom.SelectValues('//div[contains(h5/text(), "Author(s)") or contains(h5/text(), "Auth.")]/following-sibling::div//a')
+    info.Artist = dom.SelectValues('//div[contains(h5/text(), "Artist(s)") or contains(h5/text(), "Artist")]/following-sibling::div//a')
+    info.Tags = dom.SelectValues('//div[contains(h5/text(), "Genre(s)") or contains(h5/text(), "Genres")]/following-sibling::div//a')
     info.Type = dom.SelectValue('//div[contains(h5/text(), "Type")]/following-sibling::div')
     info.DateReleased = dom.SelectValue('//div[contains(h5/text(), "Release")]/following-sibling::div')
     info.Status = dom.SelectValue('//div[contains(h5/text(), "Status")]/following-sibling::div')
@@ -25,6 +26,18 @@ function GetInfo()
 
     if(module.GetName(url):endswith('Scans')) then
         info.Scanlator = module.GetName(url)
+    end
+
+    -- Sometimes we need to get the title in a different way (www.porncomixonline.net).
+
+    if(isempty(info.Title)) then
+        info.Title = dom.SelectValue('//h3')
+    end
+
+    -- Reader galleries don't always have a title, so we'll use the title of the selected chapter (www.porncomixonline.net).
+
+    if(isempty(info.Title)) then
+        info.Title = dom.SelectValue('//li[@class="active"]')
     end
 
 end
@@ -39,6 +52,20 @@ end
 
 function GetPages()
 
-    pages.AddRange(dom.SelectValues('//div[contains(@class, "reading-content")]//img/@data-src'))
+    src = http.Get(url)
+
+    -- Sometimes the images are stored in an array (www.porncomixonline.net).
+
+    local imageArrayStr = src:regex('var\\s*chapter_preloaded_images\\s*=\\s*(\\[.+?\\])', 1)
+
+    if(not isempty(imageArrayStr)) then
+
+        pages.AddRange(Json.New(imageArrayStr))
+
+    else
+
+        pages.AddRange(dom.SelectValues('//div[contains(@class, "reading-content")]//img/@data-src'))
+
+    end
 
 end
