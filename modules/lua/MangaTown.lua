f74bc13c402020abf57109154016e3b2a4db352b
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2020-04-19 00:24:48 -0600

    Updates for mangatown.com (updated algorithms)

---
+++ modules/lua/MangaTown.lua
@@ -0,0 +1,74 @@
+function Register()
+
+    module.Name = 'MangaTown'
+    module.Language = 'English'
+
+    module.Domains.Add('mangatown.com', 'MangaTown')
+
+end
+
+function GetInfo()
+
+    info.Title = dom.SelectValue('//h1')
+    info.AlternativeTitle = dom.SelectValue('//b[contains(text(),"Alternative Name")]/following-sibling::text()'):split(';')
+    info.Tags = dom.SelectValues('//b[contains(text(),"Genre")]/following-sibling::a')
+    info.Author = dom.SelectValues('//b[contains(text(),"Author")]/following-sibling::a')  
+    info.Artist = dom.SelectValues('//b[contains(text(),"Artist")]/following-sibling::a')
+    info.Status = dom.SelectValues('//b[contains(text(),"Status")]/following-sibling::text()[1]')
+    info.Type = dom.SelectValues('//b[contains(text(),"Type")]/following-sibling::a')
+    info.Summary = dom.SelectValue('//b[contains(text(),"Summary")]/following-sibling::span[@id="show"]/text()[1]')
+
+    local pageCount = ParsePageCount()
+
+    if(not isempty(pageCount)) then
+        info.PageCount = pageCount
+    end
+
+end
+
+function GetChapters()
+
+    for node in dom.SelectElements('//ul[contains(@class,"chapter_list")]/li') do
+        
+        local chapterUrl = node.SelectValue('a/@href')
+        local chapterTitle = node.SelectValue('a')
+        local chapterSubtitle = node.SelectValue('span[not(@class)]')
+
+        -- Some chapters have a subtitle, but not all of them.
+
+        if(not isempty(chapterSubtitle)) then
+            chapterTitle = chapterTitle .. ' - ' .. chapterSubtitle
+        end
+
+        chapters.Add(chapterUrl, chapterTitle)
+
+    end
+
+    chapters.Reverse()
+
+end
+
+function GetPages()
+
+    local pageCount = tonumber(ParsePageCount())
+
+    for i = 1, pageCount do
+
+        local imageUrl = dom.SelectValue('//div[contains(@class,"read_img")]/a/img/@src')
+        local nextPageUrl = dom.SelectValue('//a[contains(@class,"next_page")]/@href')
+
+        pages.Add(imageUrl)
+
+        if(not isempty(nextPageUrl)) then
+            dom = Dom.New(http.Get(nextPageUrl))
+        end
+
+    end
+
+end
+
+function ParsePageCount()
+
+    return dom.SelectValue('//div[contains(@class,"page_select")]/select/option[last()-1]/text()')
+
+end
