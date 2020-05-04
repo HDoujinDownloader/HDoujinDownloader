f363cfd0ce0581f360f4680780e3945b86af3736
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2020-05-04 02:37:56 -0600

    Added support for earlymanga.net (NON-H)

---
+++ modules/lua/EarlyManga.lua
@@ -0,0 +1,37 @@
+require "Madara"
+
+function Register()
+
+    module.Name = 'EarlyManga'
+    module.Language = 'English'
+
+    module.Domains.Add('earlymanga.net', 'EarlyManga')
+    module.Domains.Add('earlymanga.website', 'EarlyManga')
+
+end
+
+function GetChapters()
+
+    -- We need to make a POST request to get the chapters list.
+
+    local mangaParameters = tostring(dom):regex('var\\s*manga\\s*=\\s*({.+?};)', 1)
+    local mangaJson = Json.New(mangaParameters)  
+
+    if(isempty(mangaJson['chapter_slug'])) then
+
+        http.Headers['x-requested-with'] = 'XMLHttpRequest'
+    
+        http.PostData['action'] = 'manga_get_chapters'
+        http.PostData['manga'] = mangaJson['manga_id']
+    
+        local endpoint = 'https://'..module.Domain..'/wp-admin/admin-ajax.php'
+    
+        dom = Dom.New(http.Post(endpoint))
+    
+        chapters.AddRange(dom.SelectElements('//li[contains(@class,"wp-manga-chapter")]/a'))
+    
+        chapters.Reverse()
+
+    end
+
+end
