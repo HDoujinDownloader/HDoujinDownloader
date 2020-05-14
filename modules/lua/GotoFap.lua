a8864330fbfca3c6a63577d99e9d6e8be035e442
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2020-05-14 01:46:58 -0600

    Updates for gotofap.org (updated algorithms)

--- modules/lua/GotoFap.lua
+++ modules/lua/GotoFap.lua
@@ -19,17 +19,19 @@ function GetInfo()
     info.Tags = dom.SelectValues('//name_tag[contains(text(), "Tags")]/following-sibling::info_tag//a')
     info.PageCount = dom.SelectValue('//name_tag[contains(text(), "Length")]/following-sibling::info_tag')
 
+    -- Access the reader by following the link from the first thumbnail.
+
+    info.Url = dom.SelectValue('//div[contains(@class, "gallery-icon")]/a/@href')
+
 end
 
 function GetPages()
 
     -- There is no consistent relationship between thumbnail URLs and full image URLs, so we need to get each page from the reader.
 
-    for readerPageUrl in dom.SelectValues('//div[contains(@class, "gallery-icon")]/a/@href') do
+    for page in Paginator.New(http, dom, '//a[img[contains(@class,"btn_next")]]/@href') do
 
-        dom = Dom.New(http.Get(readerPageUrl))
-
-        pages.Add(dom.SelectValue('//div[contains(@class,"entry-content")]//img/@src'))
+        pages.Add(page.SelectValue('//div[contains(@class,"entry-content")]//img/@src'))
 
     end
 
