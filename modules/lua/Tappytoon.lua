af7855411730d769a0a3b583aed8ffdd47dc4afb
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2020-11-16 08:59:30 -0800

    Added support for tappytoon.com (NON-H)

---
+++ modules/lua/Tappytoon.lua
@@ -0,0 +1,82 @@
+function Register()
+
+    module.Name = 'Tappytoon'
+    module.Type = 'Webtoon'
+    
+    module.Domains.Add('tappytoon.com')
+
+end
+
+function GetInfo()
+
+    local json = GetGalleryJson()
+    local comicJson = json.SelectToken('..pageProps.comic')
+
+    if(not isempty(comicJson)) then
+
+        info.Title = comicJson['title']
+        info.Summary = comicJson['longDescription']
+        info.Tags = comicJson.SelectValues('genres[*].name')
+        info.Author = comicJson.SelectValues('authors[*].name')
+
+    end
+
+    info.Language = json.SelectValues('..locale').First()
+    
+    -- If a chapter URL was added, we might not have a title.
+
+    if(isempty(info.Title)) then
+        info.Title = tostring(dom.Title):beforelast('-')
+    end
+
+end
+
+function GetChapters()
+
+    local json = GetGalleryJson()
+    local chaptersJson = json.SelectToken('..entities.chapters')
+    local comicId = json.SelectValue('..pageProps.comic.id')
+    local baseUrl = url:before('/comics/')..'/'
+
+    for chapterId in json.SelectValues('..get-chapters-by-comic-id-'..comicId..'.response.data.result[*]') do
+
+        local chapterJson = chaptersJson.SelectToken(chapterId)
+        local chapterUnlocked = toboolean(chapterJson['isUserUnlocked']) or toboolean(chapterJson['isFree']) or toboolean(chapterJson['isUserRented'])
+
+        if(chapterUnlocked) then
+
+            local chapter = ChapterInfo.New()
+
+            chapter.Title = chapterJson['title']
+            chapter.Url = baseUrl..'chapters/'..chapterId
+    
+            chapters.Add(chapter)
+
+        end
+
+    end
+
+end
+
+function GetPages()
+
+    local json = GetGalleryJson()
+    local locale = json.SelectValues('..locale').First()
+    local chapterId = json.SelectValues('..chapterId').First()
+    local headersJson = json.SelectToken('..initialState.axios.headers')
+
+    for node in headersJson do
+        http.Headers[node.Key] = tostring(headersJson[node.Key])
+    end
+    
+    local apiResponse = http.Get('https://api-global.tappytoon.com/chapters/'..chapterId..'?includes=images&locale='..locale)
+
+    pages.AddRange(Json.New(apiResponse).SelectValues('images[*].url'))
+
+end
+
+function GetGalleryJson()
+
+    return Json.New(dom.SelectValue('//script[@id="__NEXT_DATA__"]'))    
+
+end
