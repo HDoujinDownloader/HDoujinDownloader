14b365c89a51a592565e0405710af3e5fa8d8564
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2020-12-05 09:14:46 -0800

    Updates for manganelo.today (moved to manganelos.com)

--- modules/lua/EndUserEManga.lua
+++ modules/lua/EndUserEManga.lua
@@ -5,45 +5,38 @@ function Register()
     module.Name = 'eManga (EndUser)'
     module.Language = 'English'
 
-    module.Domains.Add('manganelo.today', 'MangaNelo.Today')
     module.Domains.Add('manganelo.online', 'MangaNelo.online')
+    module.Domains.Add('manganelo.today', 'MangaNelo.Today')
+    module.Domains.Add('manganelos.com', 'MangaNelos.com')
 
 end
 
 function GetInfo()
    
-    info.Title = dom.GetElementsByTagName('h1')[0]
-
-    if(GetDomain(url) == 'manganelo.today') then
-
-        info.Summary = dom.SelectValue('//div[@id="noidungm"]/text()')
-        info.AlternativeTitle = dom.SelectValue('//li[descendant::b[contains(text(),"Alternative")]]/text()[last()]')
-        info.Author = dom.SelectValue('//li[descendant::b[contains(text(),"Author")]]/text()[last()]'):after(':')
-        info.Status = dom.SelectValue('//li[descendant::b[contains(text(),"Status")]]/text()[last()]')
-        info.Tags = dom.SelectValues('//li[descendant::b[contains(text(),"Genres")]]//a/text()')
-
-    elseif(GetDomain(url) == 'manganelo.online') then
-
-        info.Summary = dom.SelectValue('//div[contains(@class,"manga-content")]/text()')
-        info.AlternativeTitle = dom.SelectValue('//p[descendant::span[contains(text(),"Alternative")]]/text()[last()]')
-        info.Author = dom.SelectValue('//p[descendant::span[contains(text(),"Author")]]/text()[last()]')
-        info.Type = dom.SelectValue('//p[descendant::span[contains(text(),"Type")]]/text()[last()]')
-        info.Status = dom.SelectValue('//p[descendant::span[contains(text(),"Status")]]/text()[last()]')
-        info.Tags = dom.SelectValues('//p[descendant::span[contains(text(),"Genre")]]/a/text()')
-
-    end
+    info.Title = CleanTitle(dom.SelectValue('//h1'))
+    info.AlternativeTitle = dom.SelectValue('//span[contains(text(),"Alternative")]/following-sibling::text()[1]')
+    info.Author = dom.SelectValue('//span[contains(text(),"Author")]/following-sibling::text()[1]')
+    info.Artist = dom.SelectValue('//span[contains(text(),"Artist")]/following-sibling::text()[1]')
+    info.Tags = dom.SelectValue('//span[contains(text(),"Genre")]/following-sibling::a')
+    info.Type = dom.SelectValue('//span[contains(text(),"Type")]/following-sibling::text()[1]')
+    info.DateReleased = dom.SelectValue('//span[contains(text(),"Release")]/following-sibling::text()[1]')
+    info.Status = dom.SelectValue('//span[contains(text(),"Status")]/following-sibling::text()[1]')
+    info.Summary = dom.SelectValue('//div[contains(@class,"manga-content")]')
 
 end
 
 function GetChapters()
 
-    if(GetDomain(url) == 'manganelo.today') then
+    local chapterNodes = dom.SelectElements('(//div[contains(@class,"chapter-list")])[2]//h4')
 
-        chapters.AddRange(dom.SelectElements('//div[@class="offzone"]/following-sibling::ul/li//a'))
+    for chapterNode in chapterNodes do
 
-    elseif(GetDomain(url) == 'manganelo.online') then
+        local chapter = ChapterInfo.New()
 
-        chapters.AddRange(dom.SelectElements('//div[@class="total-chapter"]/div[@class="chapter-list"]//a'))
+        chapter.Url = chapterNode.SelectValue('a/@href')
+        chapter.Title = chapterNode.SelectValue('a') .. chapterNode.SelectValue('span')
+
+        chapters.Add(chapter)
 
     end
 
@@ -56,5 +49,11 @@ function GetPages()
     pages.AddRange(dom.SelectValue('//p[@id="arraydata"]/text()'))
 
     pages.Referer = '' -- Hosts like mangapark.net 403 with a referer
+
+end
+
+function CleanTitle(title)
+
+    return RegexReplace(tostring(title), ' Manga$', '')        
 
 end
