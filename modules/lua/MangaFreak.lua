6b96d3cb4021b40f8a7fdc9024593d6bd5055118
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2020-12-24 18:00:53 -0800

    Updates for mangafreak.net (updated algorithms)

---
+++ modules/lua/MangaFreak.lua
@@ -0,0 +1,47 @@
+function Register()
+
+    module.Name = 'MangaFreak'
+    module.Language = 'English'
+
+    module.Domains.Add('mangafreak.net')
+
+end
+
+function GetInfo()
+
+    local infoNode = dom.SelectElement('//div[contains(@class,"manga_series_data")]')
+
+    info.Title = dom.SelectValue('//h5')
+    info.AlternativeTitle = infoNode.SelectValue('text[1]')
+    info.DateReleased = infoNode.SelectValue('div[1]')
+    info.Status = infoNode.SelectValue('div[2]')
+    info.Author = infoNode.SelectValue('div[3]')
+    info.Artist = infoNode.SelectValue('div[4]')
+    info.ReadingDirection = infoNode.SelectValue('div[5]')
+    info.Tags = dom.SelectValues('//div[contains(@class,"series_sub_genre_list")]/a')
+    info.Summary = dom.SelectValue('//p')
+
+    if(isempty(info.Title)) then
+        info.Title = CleanTitle(dom.SelectValue('//title'))
+    end
+
+end
+
+function GetChapters()
+
+   chapters.AddRange(dom.SelectElements('//div[contains(@class,"manga_series_list")]//a[not(@download)]')) 
+
+end
+
+function GetPages()
+
+    pages.AddRange(dom.SelectValues('//img[@id="gohere"]/@src'))
+
+end
+
+function CleanTitle(title)
+
+    return RegexReplace(tostring(title), '^Read|- MangaFreak$', '')
+        :trim()
+
+end
