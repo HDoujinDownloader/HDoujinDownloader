60c3952db3f95ad92f1627f713291ce7bef15e9d
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2021-01-19 00:00:17 -0700

    Added support for mangamainac WordPress theme

---
+++ modules/lua/WpMangamainac.lua
@@ -0,0 +1,56 @@
+-- Mangamainac is a WordPress theme.
+-- https://www.themedetect.com/Theme/mangamainac
+
+function Register()
+
+    module.Name = 'mangamainac'
+    module.Language = 'English'
+    module.Type = 'Manga'
+    module.Adult = false
+
+    module.Domains.Add('onepiecechapters.com', 'TCB Scans')
+
+end
+
+function GetInfo()
+
+    info.Title = CleanTitle(dom.SelectValue('//h1'))
+    info.Author = dom.SelectValue('(//*[*[contains(text(),"Author(s)")]]/following-sibling::text())[1]')
+    info.DateReleased = dom.SelectValue('(//*[*[contains(text(),"Released")]]/following-sibling::text())[1]')
+    info.Tags = dom.SelectValue('(//*[*[contains(text(),"Genre(s)")]]/following-sibling::text())[1]'):split(',')
+    info.Status = dom.SelectValue('(//*[*[contains(text(),"Status")]]/following-sibling::text())[1]'):split(' ')[0]
+    info.Description = dom.SelectValue('(//*[*[contains(text(),"Description")]]/following-sibling::text())[1]')
+
+end
+
+function GetChapters()
+
+    chapters.AddRange(dom.SelectElements('//ul[contains(@class,"maniac_posts")]//a'))
+
+    chapters.Reverse()
+
+end
+
+function GetPages()
+
+    for pageUrl in dom.SelectValues('//div[contains(@class,"img_container")]//img/@src') do
+
+        -- Skip the Twitter advertisement banner.
+
+        if(not pageUrl:startsWith('https://cdn.imageart.online/')) then
+
+            pages.Add(pageUrl)
+
+        end
+
+    end
+
+end
+
+function CleanTitle(title) 
+
+    title = tostring(title):trim()
+
+    return RegexReplace(title, 'Manga$', '')
+
+end
