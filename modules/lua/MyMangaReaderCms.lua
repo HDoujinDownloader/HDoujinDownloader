82a97f7cc582f3ff7da44eedb49e788c3935356e
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2021-01-21 02:25:27 -0700

    Added support for manhwas.men

--- modules/lua/MyMangaReaderCms.lua
+++ modules/lua/MyMangaReaderCms.lua
@@ -5,6 +5,16 @@ function Register()
 
     module.Name = 'my Manga Reader CMS'
 
+    -- Korean
+
+    module = Module.New()
+
+    module.Language = 'Korean'
+
+    module.Domains.Add('manhwas.men', 'MANHWAS MEN')
+    
+    RegisterModule(module)
+
     -- Spanish
 
     module = Module.New()
@@ -19,15 +29,17 @@ end
 
 function GetInfo()
 
-    info.Title = dom.SelectValue('//h2')
+    info.Title = dom.SelectValue('//h2[contains(@class,"widget-title")]')
     info.Status = dom.SelectValue('//div[contains(@class,"manga-name")]/a')
-    info.Type = dom.selectValue('//dt[contains(text(), "Tipo")]/following-sibling::dd')
+    info.Type = dom.selectValue('//dt[contains(text(), "Tipo") or contains(text(), "Type")]/following-sibling::dd')
     info.OriginalTitle = dom.selectValue('//dt[contains(text(), "Nombres")]/following-sibling::dd')
+    info.AlternativeTitle = dom.selectValue('//dt[contains(text(), "Other names")]/following-sibling::dd')
     info.Author = dom.selectValue('//dt[contains(text(), "Autor")]/following-sibling::dd')
     info.Artist = dom.selectValue('//dt[contains(text(), "Artista")]/following-sibling::dd')
     info.DateReleased = dom.selectValue('//dt[contains(text(), "Publicación")]/following-sibling::dd')
     info.Tags = dom.selectValues('//dt[contains(text(), "Género")]/following-sibling::dd//a')
     info.Adult = not isempty(dom.SelectValue('//i[contains(@class,"adult")]'))
+    info.Summary = dom.SelectValues('//h5/following-sibling::p'):join('\n')
 
     if(isempty(info.Title)) then
         info.Title = dom.Title:before(' - ')
@@ -37,7 +49,20 @@ end
 
 function GetChapters()
 
-    chapters.AddRange(dom.SelectElements('//ul[contains(@class,"chapters")]//i/a'))
+    for chapterNode in dom.SelectElements('//ul[contains(@class,"chapters")]//h5') do
+
+        local chapterTitle = tostring(chapterNode)
+        local chapterUrl = chapterNode.SelectValue('i/a/@href') -- manhwas.men
+
+        if(isempty(chapterUrl)) then
+            chapterUrl = chapterNode.SelectValue('a/@href')
+        end
+
+        chapters.Add(chapterUrl, chapterTitle)
+
+    end
+
+    chapters.Reverse()
 
 end
 
@@ -46,16 +71,27 @@ function GetPages()
     local baseUrl = tostring(dom):regex('base_url\\s*=\\s*\"([^"]+)', 1)
     local images = tostring(dom):regex('pages\\s*=\\s*(\\[[^]]+\\])', 1)
 
+    local imagesJson = Json.New(images)
+
     -- mangas.in replaces the default base_url with their own CDN.
 
     if(module.Domain == 'mangas.in') then
         baseUrl = tostring(dom):regex("[^\\/]jQuery\\('\\.scan-page'\\)\\.attr\\('src',\\s*'([^']+)", 1)
     end
 
-    baseUrl = baseUrl:trim('/')
-    
-    for image in Json.New(images).SelectValues('[*].page_image') do
-        pages.Add(baseUrl..'/'..image)
+    for image in Json.New(images) do
+
+        local imageUrl = tostring(image['page_image'])
+
+        -- On mangas.in, some content uses their own CDN, while some content uses an external CDN (like Blogspot).
+        -- When an external CDN is used, the URL is obfuscated using EncodeURIComponent and then btoa.
+
+        if(module.Domain == 'mangas.in' and tostring(image['external']) == '1') then
+            imageUrl = DecodeUriComponent(DecodeBase64(imageUrl:after('//')))
+        end
+
+        pages.Add(GetRooted(imageUrl, baseUrl))
+
     end
 
 end
