d95fb40f163635a871c74f1f9abf75dab794ac8e
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2021-02-13 03:15:26 -0700

    Updates for epikmanga.com (updated metadata)

--- modules/lua/EpikManga.lua
+++ modules/lua/EpikManga.lua
@@ -9,14 +9,14 @@ end
 
 function GetInfo()
 
-    info.Title = dom.SelectValue('//h1')
+    info.Title = dom.SelectValue('//h1/text()')
     info.Type = dom.SelectValue('//span[contains(@class, "badge")]')
     info.AlternativeTitle = dom.SelectValue('//strong[contains(text(), "Alternatif İsim")]/following-sibling::text()')
     info.Status = dom.SelectValue('//strong[contains(text(), "Durum")]/following-sibling::text()')
     info.Author = dom.SelectValue('//strong[contains(text(), "Yazar")]/following-sibling::text()')
     info.Artist = dom.SelectValue('//strong[contains(text(), "Çizer")]/following-sibling::text()')
     info.Tags = dom.SelectValues('//strong[contains(text(), "Türler")]/following-sibling::a')
-    info.Summary = dom.SelectValue('//h3/following-sibling::p')
+    info.Summary = dom.SelectValues('//h3/following-sibling::p'):join('\n')
 
 end
 
