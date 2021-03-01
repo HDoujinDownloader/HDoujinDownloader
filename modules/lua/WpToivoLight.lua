9c37bcb5cfde7a9b2b19e7916ba2250ee628a061
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2021-03-01 08:36:44 -0700

    Unnecessary text in title is now stripped

--- modules/lua/WpToivoLight.lua
+++ modules/lua/WpToivoLight.lua
@@ -6,6 +6,7 @@ function Register()
     module.Name = 'Toivo Light'
     module.Language = 'English'
 
+    module.Domains.Add('neverland-manga.com')
     module.Domains.Add('pokemon-manga.com')
     module.Domains.Add('pokemonmanga.com')
 
@@ -13,7 +14,7 @@ end
 
 function GetInfo()
 
-    info.Title = dom.SelectValue('//h1')
+    info.Title = CleanTitle(dom.SelectValue('//h1'))
     info.Summary = dom.SelectValues('//div[contains(@class,"entry-content")]/p'):join('\n')
     info.Tags = dom.SelectValues('//span[contains(@class,"post_tag")]//a')
 
@@ -52,5 +53,11 @@ function GetPages()
         pages.AddRange(dom.SelectValues('//div[contains(@class,"entry-content")]//img/@src'))
 
     end
+
+end
+
+function CleanTitle(title)
+
+    return RegexReplace(tostring(title):trim(), '(?i)\\s*(?:manga online)$', '')
 
 end
