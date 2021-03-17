661d5607f9d827bbf9ebcf15f6b83d5eb7d3ca85
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2021-03-17 01:17:47 -0600

    Added support for nightcomic.com

--- modules/lua/MadaraAjax.lua
+++ modules/lua/MadaraAjax.lua
@@ -16,6 +16,7 @@ function Register()
     module.Domains.Add('mangarockteam.com', 'Manga Rock Team')
     module.Domains.Add('manhuaplus.com', 'ManhuaPLus')
     module.Domains.Add('manhuaus.com', 'Manhuaus.com')
+    module.Domains.Add('nightcomic.com', 'Night Comic')
 
 end
 
@@ -29,11 +30,11 @@ function GetChapters()
     if(isempty(mangaJson['chapter_slug'])) then
 
         http.Headers['x-requested-with'] = 'XMLHttpRequest'
-    
+
         http.PostData['action'] = 'manga_get_chapters'
         http.PostData['manga'] = mangaJson['manga_id']
     
-        local endpoint = 'https://'..module.Domain..'/wp-admin/admin-ajax.php'
+        local endpoint = 'https://'..GetHost(url)..'/wp-admin/admin-ajax.php'
     
         dom = Dom.New(http.Post(endpoint))
     
