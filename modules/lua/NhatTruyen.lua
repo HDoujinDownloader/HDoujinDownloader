38c19082d3a1fb62cd590efe05275e875b4b4a8a
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2021-03-23 02:28:44 -0600

    Added support for nhattruyen.com

---
+++ modules/lua/NhatTruyen.lua
@@ -0,0 +1,31 @@
+function Register()
+   
+    module.Name = 'NhatTruyen'
+    module.Language = 'Vietnamese'
+
+    module.Domains.Add('nhattruyen.com')
+    
+end
+
+function GetInfo()
+
+    info.Title = dom.SelectValue('//h1')
+    info.Author = dom.SelectValue('//p[contains(.,"Tác giả")]/following-sibling::p')
+    info.Tags = dom.SelectValues('//p[contains(.,"Thể loại")]/following-sibling::p//a')
+    info.Summary = dom.SelectValue('//div[contains(@class,"detail-content")]//p')
+
+end
+
+function GetChapters()
+
+    chapters.AddRange(dom.SelectElements('//div[@id="nt_listchapter"]//div[contains(@class,"chapter")]/a'))
+
+    chapters.Reverse()
+
+end
+
+function GetPages()
+
+    pages.AddRange(dom.SelectValues('//div[contains(@id,"page_")]/img/@data-original'))
+
+end
