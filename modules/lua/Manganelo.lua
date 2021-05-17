c8c241a3dc8fa5860d3b4ee6d678888b1acfe66a
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2021-05-17 16:15:43 -0600

    Updates for manganelo.com (now tries alternative image servers)

--- modules/lua/Manganelo.lua
+++ modules/lua/Manganelo.lua
@@ -10,6 +10,9 @@ function Register()
     module.Domains.Add('mangakakalot.tv', 'Mangakakalot')
     module.Domains.Add('manganelo.com', 'Manganelo')
     module.Domains.Add('manganelo.tv', 'Manganelo')
+
+    module.Settings.AddCheck('Try other image servers', true)
+        .WithToolTip('If enabled, the image URLs from the alternative image server(s) will be used as backup.')
     
 end
 
@@ -60,21 +63,37 @@ end
 
 function GetPages()
 
-    pages.AddRange(dom.SelectValues('//div[contains(@class, "chapter-reader") or contains(@class, "vung-doc") or contains(@class, "vung_doc")]/img/@src'))
+    -- Get all image URLs for the current image server.
 
-    -- Update (09/03/2021): We need to use the data-src attribute for Manganelo (manganelo.tv).
+    pages.AddRange(GetImageUrls())
 
-    if(isempty(pages)) then
-        pages.AddRange(dom.SelectValues('//div[contains(@class, "chapter-reader") or @id="vungdoc"]/img/@data-src'))
-    end
+    -- Add the image URLs from the other servers as backup URLs.
+    -- Which server the images are loaded from is dependent upon the value of the "content_server" cookie.
+    -- For example, the second image server is used if "content_server" is "server2".
 
-    -- Update (23/03/2021): The images are stored in an array on mangakakalot.city.
+    if(toboolean(module.Settings['Try other image servers'])) then
 
-    if(isempty(pages)) then
+        local contentServerUrls = dom.SelectValues('//a[contains(@class,"server-image-btn")]/@data-l')
 
-        local pageArray = dom.SelectValue('//p[@id="arraydata"]')
+        if(not isempty(contentServerUrls)) then
+    
+            local contentServerUrl = contentServerUrls.First()
+    
+            -- This request sets the "content_server" cookie and updates the list of images.
+    
+            http.Get(contentServerUrl)
 
-        pages.AddRange(pageArray:split(','))
+            dom = Dom.New(http.Get(url))
+    
+            local backupImageUrls = GetImageUrls()
+            
+            for i = 0, math.min(backupImageUrls.Count(), pages.Count()) - 1 do
+
+                pages[i].BackupUrls.Add(backupImageUrls[i])
+
+            end
+            
+        end
 
     end
 
@@ -93,5 +112,31 @@ function FollowRedirect()
     if(not isempty(redirectUrl)) then
         dom = Dom.New(http.Get(redirectUrl))
     end
+
+end
+
+function GetImageUrls()
+
+    local imageList = List.New()
+
+    imageList.AddRange(dom.SelectValues('//div[contains(@class, "chapter-reader") or contains(@class, "vung-doc") or contains(@class, "vung_doc")]/img/@src'))
+
+    -- Update (09/03/2021): We need to use the data-src attribute for Manganelo (manganelo.tv).
+
+    if(isempty(imageList)) then
+        imageList.AddRange(dom.SelectValues('//div[contains(@class, "chapter-reader") or @id="vungdoc"]/img/@data-src'))
+    end
+
+    -- Update (23/03/2021): The images are stored in an array on mangakakalot.city.
+
+    if(isempty(imageList)) then
+
+        local pageArray = dom.SelectValue('//p[@id="arraydata"]')
+
+        imageList.AddRange(pageArray:split(','))
+
+    end
+
+    return imageList
 
 end
