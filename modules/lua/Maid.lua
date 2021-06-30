b8bf9762692bfc60734a46ea95971f1e492d0ee6
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2021-06-29 19:53:40 -0600

    Added support for maid.my.id

---
+++ modules/lua/Maid.lua
@@ -0,0 +1,53 @@
+function Register()
+
+    module.Name = 'Maid'
+    module.Language = 'Indonesian'
+
+    module.Domains.Add('maid.my.id')
+    module.Domains.Add('www.maid.my.id')
+
+end
+
+function GetInfo()
+
+    info.Title = dom.SelectValue('//h2')
+    info.OriginalTitle = dom.SelectValue('//h2/following-sibling::span')
+    info.Tags = dom.SelectValues('//div[contains(@class,"series-genres")]/a')
+    info.Summary = dom.SelectValue('//div[contains(@class,"series-synops")]')
+    info.DateReleased = dom.SelectValue('//span[contains(@class,"published")]')
+
+    for author in dom.SelectValue('//span[contains(@class,"author")]'):split(',') do
+
+        local authorName = author:before('(')
+        local authorRole = author:between('(', ')'):lower()
+
+        if(authorRole == 'story') then
+            info.Author = info.Author .. authorName .. ','
+        elseif(authorRole == 'art') then
+            info.Artist = info.Artist .. authorName .. ','
+        end
+
+    end
+
+end
+
+function GetChapters()
+
+    for chapterNode in dom.SelectElements('//ul[contains(@class,"chapterlist")]//a[@title]') do
+
+        local chapterUrl = chapterNode.SelectValue('@href')
+        local chapterTitle = chapterNode.SelectValue('span[1]')
+
+        chapters.Add(chapterUrl, chapterTitle)
+
+    end
+
+    chapters.Reverse()
+
+end
+
+function GetPages()
+
+    pages.AddRange(dom.SelectValues('//div[contains(@class,"reader-area")]//img/@src'))
+
+end
