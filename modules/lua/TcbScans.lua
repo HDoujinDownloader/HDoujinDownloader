279abc59532b95b498c71575ae0c97bd3122cf7a
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2021-11-07 18:53:27 -0700

    Fixed download for TCB Scans

---
+++ modules/lua/TcbScans.lua
@@ -0,0 +1,42 @@
+function Register()
+
+    module.Name = 'TCB Scans'
+    module.Language = 'English'
+
+    module.Domains.Add('onepiecechapters.com')
+
+end
+
+function GetInfo()
+
+    info.Title = dom.SelectValue('//h1')
+    info.Summary = dom.SelectValue('//h1/following-sibling::p')
+    info.Scanlator = 'TCB Scans'
+
+end
+
+function GetChapters()
+
+    for chapterNode in dom.SelectElements('//div[contains(text(),"Chapters")]/following-sibling::a') do
+
+        local chapterUrl = chapterNode.SelectValue('@href')
+        local chapterTitle = chapterNode.SelectValue('./div[1]')
+        local chapterSubtitle = chapterNode.SelectValue('./div[2]')
+
+        if(not isempty(chapterSubtitle)) then
+            chapterTitle = chapterTitle .. ' - ' .. chapterSubtitle
+        end
+
+        chapters.Add(chapterUrl, chapterTitle)
+
+    end
+
+    chapters.Reverse()
+
+end
+
+function GetPages()
+
+    pages.AddRange(dom.SelectValues('//picture/img/@src'))
+
+end
