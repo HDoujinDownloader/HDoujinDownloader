bfae0652585bf5aac05cfde5a733e8343e0e87d7
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2021-11-07 18:06:23 -0700

    Fixed downloads for manhwa18.com

---
+++ modules/lua/Manhua18.lua
@@ -0,0 +1,41 @@
+function Register()
+
+    module.Name = 'Manhua18'
+    module.Adult = true
+    module.Language = 'English'
+
+    module.Domains.Add('manhwa18.com')
+
+end
+
+function GetInfo()
+
+    info.Title = dom.SelectValue('//span[contains(@class,"series-name")]')
+    info.AlternativeTitle = dom.SelectValue('//span[contains(text(),"Other name")]/following-sibling::span')
+    info.Tags = dom.SelectValues('//span[contains(text(),"Genre")]/following-sibling::span/a')
+    info.Author = dom.SelectValues('//span[contains(text(),"Author")]/following-sibling::span/a')
+    info.Status = dom.SelectValues('//span[contains(text(),"Status")]/following-sibling::span/a')
+    info.Summary = dom.SelectValue('//div[contains(@class,"summary-content")]')
+
+end
+
+function GetChapters()
+
+    for chapterNode in dom.SelectElements('//ul[contains(@class,"list-chapters")]/a') do
+
+        local chapterUrl = chapterNode.SelectValue('@href')
+        local chapterTitle = chapterNode.SelectValue('.//div[contains(@class,"chapter-name")]')
+
+        chapters.Add(chapterUrl, chapterTitle)
+
+    end
+
+    chapters.Reverse()
+
+end
+
+function GetPages()
+
+    pages.AddRange(dom.SelectValues('//div[@id="chapter-content"]//img/@data-src'))
+
+end
