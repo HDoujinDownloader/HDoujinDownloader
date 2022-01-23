6d0541f37bb14c3d279f40e6eb0f19219930911e
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2022-01-22 19:41:27 -0700

    ninemanga.com: fixed download

---
+++ modules/lua/NineManga.lua
@@ -0,0 +1,46 @@
+function Register()
+
+    module.Name = 'NineManga'
+    module.Language = 'Spanish'
+    module.Adult = false
+
+    module.Domains.Add('br.ninemanga.com')
+    module.Domains.Add('de.ninemanga.com')
+    module.Domains.Add('es.ninemanga.com')
+    module.Domains.Add('fr.ninemanga.com')
+    module.Domains.Add('it.ninemanga.com')
+    module.Domains.Add('ninemanga.com')
+    module.Domains.Add('ru.ninemanga.com')
+
+end
+
+function GetInfo()
+
+    info.Title = CleanTitle(dom.SelectValue('//h1'))
+    info.Tags = dom.SelectValues('//li[contains(@itemprop,"genre")]/a')
+    info.Status = dom.SelectValue('//li/a[contains(@class,"red")]')
+    info.Summary = dom.SelectValue('//p/b/following-sibling::text()')
+
+end
+
+function GetChapters()
+
+    chapters.AddRange(dom.SelectElements('//a[contains(@class,"chapter_list")]'))
+
+    chapters.Reverse()
+
+end
+
+function GetPages()
+
+    local imagesJson = Json.New(dom.SelectValue('//script[contains(.,"all_imgs_url")]'):regex('all_imgs_url:\\s*(\\[[^\\]]+\\])', 1))
+
+    pages.AddRange(imagesJson)
+
+end
+
+function CleanTitle(title)
+
+    return RegexReplace(tostring(title), '(?:Manga)$', '')
+
+end
