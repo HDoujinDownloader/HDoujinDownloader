8a6e1a4f3c2ce624b50f58a1c7c586c172df6c90
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2022-01-22 19:18:48 -0700

    mangas.in: fixed chapters

---
+++ modules/lua/MangasIN.lua
@@ -0,0 +1,27 @@
+require "MyMangaReaderCms"
+
+function Register()
+
+    module.Name = 'Mangas.in'
+    module.Language = 'Spanish'
+    module.Adult = false
+
+    module.Domains.Add('mangas.in')
+
+end
+
+function GetChapters()
+
+    for chapterNode in dom.SelectElements('//ul[contains(@class,"chapters")]//h5') do
+
+        local chapterTitle = chapterNode.SelectValue('a/following-sibling::text()')
+        local chapterSubtitle = chapterNode.SelectValue('eee')
+        local chapterUrl = chapterNode.SelectValue('eee/a/@href')
+
+        chapters.Add(chapterUrl, CleanTitle(chapterTitle .. chapterSubtitle))
+
+    end
+
+    chapters.Reverse()
+
+end
