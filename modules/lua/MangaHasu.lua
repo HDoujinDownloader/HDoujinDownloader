5aa1c368af2585c3f5e3facd9bc8d99f3126c114
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2022-03-10 13:37:06 -0700

    mangahasu.se: fixed download

---
+++ modules/lua/MangaHasu.lua
@@ -0,0 +1,37 @@
+function Register()
+
+    module.Name = 'MangaHasu'
+    module.Language = 'en'
+    module.Adult = false    
+
+    module.Domains.Add('mangahasu.com')
+    module.Domains.Add('mangahasu.se')
+
+end
+
+function GetInfo()
+
+    info.Title = dom.SelectValue('//h1')
+    info.AlternativeTitle = dom.SelectValue('//div[contains(@class,"info-title")]/h3')
+    info.Author = dom.SelectValues('//b[contains(text(),"Author(s)")]/following-sibling::span//a')
+    info.Artist = dom.SelectValues('//b[contains(text(),"Artist(s)")]/following-sibling::span//a')
+    info.Type = dom.SelectValue('//b[contains(text(),"Type")]/following-sibling::span//a')
+    info.Tags = dom.SelectValues('//b[contains(text(),"Genre(s)")]/following-sibling::span//a')
+    info.Status = dom.SelectValue('//b[contains(text(),"Status")]/following-sibling::span//a')
+    info.Summary = dom.SelectValue('//h3[contains(text(),"Summary")]/following-sibling::*')
+
+end
+
+function GetChapters()
+
+    chapters.AddRange(dom.SelectElements('//div[contains(@class,"list-chapter")]//a'))
+
+    chapters.Reverse()
+
+end
+
+function GetPages()
+
+    pages.AddRange(dom.SelectValues('//img[contains(@class,"page")]/@data-src'))
+
+end
