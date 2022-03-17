309cb6dd264e731d9ee270f656e1588f7a679b72
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2022-03-16 22:01:02 -0600

    Added support for LANraragi instances

---
+++ modules/lua/Lanraragi.lua
@@ -0,0 +1,72 @@
+function Register()
+
+    module.Name = 'LANraragi'
+
+    module.Domains.Add('hentai.fakku.cc', 'HVNC')
+
+end
+
+function GetInfo()
+
+    local json = GetGalleryJson()
+
+    info.Title = json.SelectValue('title')
+    info.Tags = json.SelectValue('tags'):split(',')
+    info.PageCount = json.SelectValue('pagecount')
+
+end
+
+function GetPages()
+
+    local json = GetImagesJson()
+
+    pages.AddRange(json.SelectValues('pages[*]'))
+
+end
+
+local function GetApiUrl()
+
+    return  '/api/archives/'
+
+end
+
+local function SetUpApiUrl(path)
+
+    return GetApiUrl() .. path
+
+end
+
+local function GetApiJson(path)
+
+    http.Headers['accept'] = '*/*'
+
+    local endpoint = SetUpApiUrl(path)
+    local json = Json.New(http.Get(endpoint))
+
+    return json
+
+end
+
+local function GetGalleryId()
+
+    return GetParameter(url, 'id')
+
+end
+
+function GetGalleryJson()
+
+    local galleryId = GetGalleryId()
+    local endpoint = galleryId .. '/metadata'
+
+    return GetApiJson(endpoint)
+
+end
+
+function GetImagesJson()
+
+    local galleryId = GetGalleryId()
+    local endpoint = galleryId .. '/files?force=false'
+
+    return GetApiJson(endpoint)
+
+end
