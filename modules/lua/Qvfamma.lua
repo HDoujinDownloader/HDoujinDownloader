b63393ce639dda9f2730cf0422e2adc10f0c232e
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2022-03-19 17:05:42 -0600

    Added support for qvfammaonline.blogspot.com

---
+++ modules/lua/Qvfamma.lua
@@ -0,0 +1,52 @@
+function Register()
+
+    module.Name = 'Qvfamma'
+    module.Language = 'Spanish'
+
+    module.Domains.Add('qvfammaonline.blogspot.com')
+
+end
+
+function GetInfo()
+
+    info.Title = dom.SelectValue('//h1[contains(@class,"entry-title")]')
+    info.Author = dom.SelectValue('//i[contains(text(),"autor/arte:")]/span')
+    info.Status = dom.SelectValue('//i[contains(.,"Estado (En Japón):")]/span')
+    info.Tags = dom.SelectValue('//i[contains(.,"Género:")]/text()'):split(',')
+
+end
+
+function GetChapters()
+
+    -- Get the related posts and consider them chapters.
+    -- e.g. /2019/10/fudanshi-shokan-informacion.html
+
+    local title = dom.SelectValue('//h1[contains(@class,"entry-title")]')
+    local slug = dom.SelectValue('//a[contains(@href,"/label/")]/@href'):regex('\\/label\\/([^?&]+)', 1)
+
+    if(title:contains('[Información]') and not isempty(slug)) then
+
+        local json = Json.New(http.Get('/feeds/posts/default/-/' .. slug .. '?alt=json&max-results=15'))
+
+        for postNode in json.SelectTokens("feed.entry[*].link[?(@.type == 'text/html')]") do
+
+            local postTitle = postNode.SelectValue('title')
+            local postUrl = postNode.SelectValue('href')
+            
+            if(not postTitle:contains('[Información]')) then
+                chapters.Add(postUrl, postTitle)
+            end
+
+        end
+
+    end
+
+    chapters.Reverse()
+
+end
+
+function GetPages()
+
+    pages.AddRange(dom.SelectValues('//div[contains(@class,"entry-content")]//img/@src'))
+
+end
