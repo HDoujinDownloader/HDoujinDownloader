566ed8c3cf1362502ba14340077ae4d60b5b5ee0
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2022-03-29 12:49:04 -0600

    nettruyen.com: moved to nettruyenmoi.com

---
+++ modules/lua/NetTruyen.lua
@@ -0,0 +1,35 @@
+function Register()
+
+    module.Name = 'NetTruyen'
+    module.Language = 'vn'
+
+    module.Domains.Add('nettruyen.com')
+    module.Domains.Add('nettruyenmoi.com')
+    module.Domains.Add('www.nettruyen.com')
+    module.Domains.Add('www.nettruyenmoi.com')
+
+end
+
+function GetInfo()
+
+    info.Title = dom.SelectValue('//h1')
+    info.Author = dom.SelectValues('//p[contains(.,"Tác giả")]/following-sibling::p//a')
+    info.Status = dom.SelectValue('//p[contains(.,"Tình trạng")]/following-sibling::p')
+    info.Tags = dom.SelectValues('//p[contains(.,"Thể loại")]/following-sibling::p//a')
+    info.Summary = dom.SelectValue('//div[contains(@class,"detail-content")]//p')
+
+end
+
+function GetChapters()
+
+    chapters.AddRange(dom.SelectElements('//div[contains(@class,"chapter")]/a'))
+
+    chapters.Reverse()
+
+end
+
+function GetPages()
+
+    pages.AddRange(dom.SelectValues('//div[contains(@id,"page_")]/img/@data-original'))
+
+end
