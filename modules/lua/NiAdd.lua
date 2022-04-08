0eaee04a50db4a01dfb55d53a52545f80d7e8d51
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2022-04-08 14:10:39 -0600

    niadd.com: removed obsolete GetPages implementation

--- modules/lua/NiAdd.lua
+++ modules/lua/NiAdd.lua
@@ -1,7 +1,5 @@
 require 'NineManga'
 
-local BaseGetPages = GetPages
-
 function Register()
 
     module.Name = 'NiAdd'
@@ -53,41 +51,6 @@ function GetChapters()
 
     chapters.Reverse()
     
-end
-
-function GetPages()
-
-    -- Switch to "10 pages" mode, which allows us to access all of the pages.
-    -- We will be redirected multiple times.
-
-    url = url:trim('/') .. '-10-1.html'
-    dom = Dom.New(http.Get(url))
-
-    BaseGetPages()
-
-    -- Some of the subdomains use a different reader where the images are directly in the HTML.
-    -- We can only get 10 images at a time.
-
-    if(isempty(pages)) then
-
-        -- It will paginate into the next chapter, so make sure we only get the pages for this chapter.
-
-        local chapterSlug = url:regex('\\/chapter(\\/[^\\/]+\\/)', 1)
-
-        for page in Paginator.New(http, dom, '//div[contains(@class,"btn-next")]/a[contains(@href, "' .. chapterSlug .. '")]/@href') do
-    
-            local imageUrls = page.SelectValues('//img[contains(@class,"manga_pic")]/@src')
-
-            if(isempty(imageUrls)) then
-                break
-            end
-
-            pages.AddRange(imageUrls)
-        
-        end
-
-    end
-
 end
 
 function CleanTitle(title)
