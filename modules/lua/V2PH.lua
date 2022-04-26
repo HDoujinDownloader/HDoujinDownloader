7c5aae151737a2ed48a3bd546ee1d6fd9425b046
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2022-04-25 23:07:03 -0600

    Added support for v2ph.com

---
+++ modules/lua/V2PH.lua
@@ -0,0 +1,34 @@
+function Register()
+
+    module.Name = 'V2PH'
+    module.Type = 'photography'
+
+    module.Domains.Add('v2ph.com')
+    module.Domains.Add('www.v2ph.com')
+
+end
+
+function GetInfo()
+
+    info.Title = dom.SelectValue('//h1')
+    info.DateReleased = dom.SelectValue('//dt[contains(text(),"Release Date")]/following-sibling::dd[1]')
+    info.Characters = dom.SelectValues('//dt[contains(text(),"Album Models")]/following-sibling::dd[1]//a')
+    info.Tags = dom.SelectValues('//dt[contains(text(),"Gallery Tags")]/following-sibling::dd[1]//a')
+    info.Publisher = dom.SelectValues('//dt[contains(text(),"Graphic Vendor")]/following-sibling::dd[1]//a')
+    info.Summary = dom.SelectValue('//div[contains(@class,"album-intro-body")]')
+    info.PageCount = dom.SelectValue('//dt[contains(text(),"Photos")]/following-sibling::dd[1]')
+
+end
+
+function GetPages()
+
+    url = StripParameters(url)
+    dom = Dom.New(http.Get(url))
+
+    for page in Paginator.New(http, dom, '//a[contains(@class,"page-link") and contains(text(),"Next")]/@href') do
+    
+        pages.AddRange(page.SelectValues('//div[contains(@class,"album-photo")]//img/@data-src'))
+    
+    end
+
+end
