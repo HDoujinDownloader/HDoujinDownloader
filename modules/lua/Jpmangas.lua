60acb609e08cc3cd0c20a6f52db8dd9f562a83f9
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2022-04-25 22:55:09 -0600

    Added support for jpmangas.cc

---
+++ modules/lua/Jpmangas.lua
@@ -0,0 +1,42 @@
+function Register()
+
+    module.Name = 'Jpmangas'
+    module.Language = 'french'
+
+    module.Domains.Add('jpmangas.cc')
+
+end
+
+function GetInfo()
+
+    info.Title = dom.SelectValue('//h2')
+    info.Status = dom.SelectValue('//dt[contains(text(),"Statut")]/following-sibling::dd[1]')
+    info.AlternativeTitle = dom.SelectValue('//dt[contains(text(),"Autres noms")]/following-sibling::dd[1]')
+    info.Author =  dom.SelectValues('//dt[contains(text(),"Auteur")]/following-sibling::dd[1]//a')
+    info.Artist =  dom.SelectValues('//dt[contains(text(),"Artist")]/following-sibling::dd[1]//a')
+    info.DateReleased = dom.SelectValue('//dt[contains(text(),"Date de sortie")]/following-sibling::dd[1]')
+    info.Tags =  dom.SelectValues('//dt[contains(text(),"Tags")]/following-sibling::dd[1]//a')
+    info.Summary = dom.SelectValue('//p')
+
+end
+
+function GetChapters()
+
+    for chapterNode in dom.SelectElements('//h5[contains(@class,"chapter-title")]') do
+
+        local chapterUrl = chapterNode.SelectValue('./a/@href')
+        local chapterTitle = tostring(chapterNode)
+
+        chapters.Add(chapterUrl, chapterTitle)
+
+    end
+
+    chapters.Reverse()
+
+end
+
+function GetPages()
+
+    pages.AddRange(dom.SelectValues('//div[@id="all"]/img/@data-src'))
+
+end
