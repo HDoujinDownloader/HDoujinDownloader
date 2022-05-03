efb79e9650ad6f7f7ecc597427efc672330d5cb5
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2022-05-02 20:38:06 -0600

    Added support for novelcool.com

---
+++ modules/lua/NovelCool.lua
@@ -0,0 +1,36 @@
+require "NineManga" -- Uses the same reader
+
+function Register()
+
+    module.Name = 'Novel Cool'
+    module.Language = 'en'
+
+    module.Domains.Add('novelcool.com')
+    module.Domains.Add('www.novelcool.com')
+
+end
+
+function GetInfo()
+
+    info.Title = dom.SelectValue('//h1')
+    info.Author = dom.SelectValue('//div[contains(@class,"bookinfo-author")]')
+    info.Summary = dom.SelectValue('//div[contains(@class,"bk-summary-txt")]')
+    info.Status = dom.SelectValue('//div[contains(@class,"bk-going")]')
+    info.Tags = dom.SelectValues('//div[contains(@class,"bk-cate-item")]//a/span')
+
+end
+
+function GetChapters()
+
+    for chapterNode in dom.SelectElements('//div[contains(@class,"chp-item")]') do
+
+        local chapterUrl = chapterNode.SelectValue('.//@href')
+        local chapterTitle = chapterNode.SelectValue('.//span[contains(@class,"chapter-item-headtitle")]')
+
+        chapters.Add(chapterUrl, chapterTitle)
+
+    end
+
+    chapters.Reverse()
+
+end
