5e64b27272783bcff94b6addc73f70c15510d183
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2022-07-21 23:21:05 -0600

    comics18.org (new)

---
+++ modules/lua/WpAsapTheme.lua
@@ -0,0 +1,22 @@
+function Register()
+
+    module.Name = 'Asap Theme'
+    module.Language = 'pt'
+    module.Adult = true
+
+    module.Domains.Add('comics18.org', 'Comics18')
+
+end
+
+function GetInfo()
+
+        info.Title = dom.SelectValue('//h1')
+        info.Tags = dom.SelectValues('//div[contains(@class,"content-tags")]//a')
+
+end
+
+function GetPages()
+
+    pages.AddRange(dom.SelectValues('//div[contains(@class,"the-content")]//img/@src'))
+
+end
