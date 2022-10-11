5b25146dc44dc2b8c0a6529ed65cc94fea7ac70a
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2022-10-10 23:54:39 -0600

    reaperscans.com: fixed download

--- modules/lua/ReaperScans.lua
+++ modules/lua/ReaperScans.lua
@@ -11,25 +11,51 @@ end
 function GetInfo()
 
     info.Title = dom.SelectValue('//h1')
-    info.Summary = dom.SelectValue('//div[contains(@class,"description-summary")]//p')
+    info.Summary = dom.SelectValue('//h1[contains(text(),"Summary")]/following-sibling::p')
     info.AlternativeTitle = dom.SelectValue('//div[h5[contains(text(),"Alternative")]]/following-sibling::div')
     info.Author = dom.SelectValues('//div[contains(@class,"author-content")]/a')
     info.Artist = dom.SelectValues('//div[contains(@class,"artist-content")]/a')
     info.Tags = dom.SelectValues('//div[contains(@class,"genres-content")]/a')
     info.Type = dom.SelectValue('//div[h5[contains(text(),"Type")]]/following-sibling::div')
     info.DateReleased = dom.SelectValue('//div[h5[contains(text(),"Release")]]/following-sibling::div/a')
-    info.Status = dom.SelectValue('//div[h5[contains(text(),"Status")]]/following-sibling::div')
+    info.Status = dom.SelectValue('//dt[contains(text(),"Source Status")]/following-sibling::dd')
+
+    local chapterCount = dom.SelectValue('//span[text()="of"]/following-sibling::span')
+
+    if(not isempty(chapterCount)) then
+        info.ChapterCount = chapterCount
+    end
 
 end
 
 function GetChapters()
 
-    for chapterNode in dom.SelectElements('//div[contains(@class,"chapter-link")]') do
+    local maxPageIndex = dom.SelectElements('//span[contains(@*,"paginator-page")]').Count()
 
-        local chapterUrl = chapterNode.SelectValue('a/@href')
-        local chapterTitle = chapterNode.SelectValue('a/p')
+    if(maxPageIndex <= 0) then
+        maxPageIndex = 1
+    end
 
-        chapters.Add(chapterUrl, chapterTitle)
+    for currentPageIndex = 1, maxPageIndex do
+
+        http.Referer = url
+
+        url = SetParameter(url, 'page', currentPageIndex)
+        dom = Dom.New(http.Get(url))
+
+        local chapterNodes = dom.SelectElements('(//ul[@role="list"])[1]//a[contains(@href,"/chapters/")]')
+
+        for i = 0, chapterNodes.Count() - 1 do
+
+            local chapterNode = chapterNodes[i]
+            local chapter = ChapterInfo.New()
+
+            chapter.Url = chapterNode.SelectValue('./@href')
+            chapter.Title = chapterNode.SelectValue('.//p')
+
+            chapters.Add(chapter)
+
+        end
 
     end
 
@@ -39,6 +65,6 @@ end
 
 function GetPages()
 
-    pages.AddRange(dom.SelectValues('//img[contains(@id,"image-")]/@data-src'))
+    pages.AddRange(dom.SelectValues('//main//img/@src'))
 
 end
