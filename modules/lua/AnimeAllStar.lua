8844cd754e7cdcb467b8420432057cdb5186a86b
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2023-06-02 17:29:51 -0600

    Added support for animeallstar20.com

---
+++ modules/lua/AnimeAllStar.lua
@@ -0,0 +1,52 @@
+function Register()
+
+    module.Name = 'AnimeAllSar'
+    module.Language = 'spanish'
+
+    module.Domains.Add('animeallstar20.com')
+    module.Domains.Add('www.animeallstar20.com')
+
+end
+
+function GetInfo()
+
+    info.Title = CleanTitle(dom.SelectValue('//h3[contains(@class,"post-title")]'))
+
+end
+
+function GetChapters()
+
+    local feedUrl = dom.SelectValue('//ul[@id="listado_epis"]/script/@src'):replace('&amp;', '&')   
+    local feedScript = http.Get(feedUrl)
+    local feedJson = Json.New(feedScript:regex('lista\\((.+}})\\);', 1))
+
+    for node in feedJson.SelectTokens('feed.entry[*].link[*]') do
+
+        local rel = node.SelectValue('rel')
+
+        if(rel == 'alternate') then
+
+            local chapterTitle = CleanTitle(node.SelectValue('title'))
+            local chapterUrl = node.SelectValue('href')
+
+            chapters.Add(chapterUrl, chapterTitle)
+
+        end
+ 
+    end
+
+    chapters.Reverse()
+
+end
+
+function GetPages()
+
+    pages.AddRange(dom.SelectValues('//div[contains(@class,"post-body")]//a/img/@src'))
+
+end
+
+function CleanTitle(title)
+
+    return RegexReplace(title, '(?i)español$', '')
+
+end
