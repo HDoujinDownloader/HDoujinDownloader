5b8ff3b6079927e59725950e769fb2e7c7045542
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2023-06-10 13:25:59 -0600

    Added support for mangathailand.com

---
+++ modules/lua/MangaThailand.lua
@@ -0,0 +1,31 @@
+function Register()
+
+    module.Name = 'MangaThailand'
+    module.Language = 'thai'
+
+    module.Domains.Add('mangathailand.com')
+
+end
+
+function GetInfo()
+
+    info.Title = dom.SelectValue('//h1')
+    info.AlternativeTitle = dom.SelectValue('(//h1)[2]')
+    info.Summary = dom.SelectValues('//div[contains(@class,"theme-post-content")]//p'):join('\n\n')
+    info.Tags = dom.SelectValues('//span[contains(@class,"info__terms-list")]/a')    
+
+end
+
+function GetChapters()
+
+    chapters.AddRange(dom.SelectElements('(//div[contains(@class,"elementor-grid-1")])[1]//a'))
+
+    chapters.Reverse()
+
+end
+
+function GetPages()
+
+    pages.AddRange(dom.SelectValues('//img[contains(@class,"manga-img")]/@data-src'))
+
+end
