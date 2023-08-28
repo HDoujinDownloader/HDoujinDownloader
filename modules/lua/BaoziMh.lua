722bcc13f2d69e59f09e88e337f01db0a0166b1d
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2023-08-28 02:15:25 -0600

    Added support for baozimh.com

---
+++ modules/lua/BaoziMh.lua
@@ -0,0 +1,42 @@
+function Register()
+
+    module.Name = '包子漫畫'
+    module.Language = 'chinese'
+
+    module.Domains.Add('baozimh.com')
+    module.Domains.Add('www.baozimh.com')
+
+end
+
+function GetInfo()
+
+    info.Title = dom.SelectValue('//h1[contains(@class,"comics-detail__title")]')
+    info.Author = dom.SelectValue('//h2[contains(@class,"comics-detail__author")]')
+    info.Tags = dom.SelectValues('//div[contains(@class,"tag")]//span')
+    info.Summary = dom.SelectValue('//p')
+
+    -- Added from reader
+
+    if(isempty(info.Title)) then
+        info.Title = dom.SelectValue('//span[contains(@class,"title")]')
+    end
+
+end
+
+function GetChapters()
+
+    chapters.AddRange(dom.SelectElements('//div[contains(@id,"chapter-items")]//a'))
+
+end
+
+function GetPages()
+
+    for jsonStr in dom.SelectValues('//amp-state[contains(@id,"chapter")]//script/text()') do
+
+        local pageUrl = Json.New(jsonStr).SelectValue('url')
+
+        pages.Add(pageUrl)
+
+    end
+
+end
