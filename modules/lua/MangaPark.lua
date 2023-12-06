1902376a888347a422f35279a6687cce617d1164
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2023-12-05 18:42:13 -0700

    mangapark.net: Fixed download

--- modules/lua/MangaPark.lua
+++ modules/lua/MangaPark.lua
@@ -1,8 +1,14 @@
 require "MangaParkV3"
 
-local BaseGetInfo = GetInfo
-local BaseGetChapters = GetChapters
-local BaseGetPages = GetPages
+local V3GetInfo = GetInfo
+local V3GetChapters = GetChapters
+local V3GetPages = GetPages
+
+require "MangaParkV5"
+
+local V5GetInfo = GetInfo
+local V5GetChapters = GetChapters
+local V5GetPages = GetPages
 
 function Register()
 
@@ -20,22 +26,10 @@ end
 
 function GetInfo()
 
-    BaseGetInfo()
-
-    if(not IsMangaParkV3()) then
-
-        local json = Json.New(dom.SelectValue('//script[contains(@id,"__NEXT_DATA__")]'))
-            .SelectToken('..state.data.data')
-
-        info.Title = json.SelectValue('name')
-        info.AlternativeTitle = json.SelectValues('altNames[*]')
-        info.Author = json.SelectValues('authors[*]')
-        info.Artist = json.SelectValues('artists[*]')
-        info.Tags = json.SelectValues('genres[*]')
-        info.Status = json.SelectValue('originalStatus')
-        info.Summary = json.SelectValue('summary.code')
-        info.Type = json.SelectValue('originalLanguage')
-
+    if(IsMangaParkV3()) then
+        V3GetInfo()
+    else
+        V5GetInfo()
     end
 
 end
@@ -43,33 +37,9 @@ end
 function GetChapters()
 
     if(IsMangaParkV3()) then
-
-        BaseGetChapters()
-
+        V3GetChapters()
     else
-
-        -- We have to query the API to get the chapter list.
-
-        local comicId = GetComicId()
-        local payload = '{"query":"\\n  query get_content_comicChapterRangeList($select: Content_ComicChapterRangeList_Select) {\\n    get_content_comicChapterRangeList(\\n      select: $select\\n    ) {\\n      reqRange{x y}\\n      missing\\n      pager {x y}\\n      items{\\n        serial \\n        chapterNodes {\\n          \\n  id\\n  data {\\n    \\n\\n  id\\n  sourceId\\n\\n  dbStatus\\n  isNormal\\n  isHidden\\n  isDeleted\\n  isFinal\\n  \\n  dateCreate\\n  datePublic\\n  dateModify\\n  lang\\n  volume\\n  serial\\n  dname\\n  title\\n  urlPath\\n\\n  srcTitle srcColor\\n\\n  count_images\\n\\n  stat_count_post_child\\n  stat_count_post_reply\\n  stat_count_views_login\\n  stat_count_views_guest\\n  \\n  userId\\n  userNode {\\n    \\n  id \\n  data {\\n    \\nid\\nname\\nuniq\\navatarUrl \\nurlPath\\n\\nverified\\ndeleted\\nbanned\\n\\ndateCreate\\ndateOnline\\n\\nstat_count_chapters_normal\\nstat_count_chapters_others\\n\\nis_adm is_mod is_vip is_upr\\n\\n  }\\n\\n  }\\n\\n  disqusId\\n\\n\\n  }\\n\\n          sser_read\\n        }\\n      }\\n\\n    }\\n  }\\n  ","variables":{"select":{"comicId":' .. comicId .. ',"range":null,"isAsc":false}},"operationName":"get_content_comicChapterRangeList"}'
-        local chaptersJson = GetApiJson(payload)
-
-        for chapterNode in chaptersJson.SelectTokens('..chapterNodes[*].data') do
-
-            local chapterInfo = ChapterInfo.New()
-
-            chapterInfo.Volume = chapterNode.SelectValue('volume')
-            chapterInfo.Title = chapterNode.SelectValue('dname')
-            chapterInfo.Url = chapterNode.SelectValue('urlPath')
-            chapterInfo.Version = chapterNode.SelectValue('srcTitle')
-            chapterInfo.Language = chapterNode.SelectValue('lang')
-
-            chapters.Add(chapterInfo)
-
-        end
-
-        chapters.Reverse()
-
+        V5GetChapters()
     end  
 
 end
@@ -77,25 +47,9 @@ end
 function GetPages()
 
     if(IsMangaParkV3()) then
-
-        BaseGetPages()
-
+        V3GetPages()
     else
-
-        local json = Json.New(dom.SelectValue('//script[contains(@id,"__NEXT_DATA__")]'))
-            .SelectToken('..imageSet')
-
-        local fileNames = json.SelectValues('httpLis[*]')
-        local accessTokens = json.SelectValues('wordLis[*]')
-
-        for i = 0, fileNames.Count() - 1 do
-
-            local imageUrl = fileNames[i] .. '?' .. accessTokens[i]
-
-            pages.Add(imageUrl)
-
-        end
-
+        V5GetPages()
     end
 
 end
@@ -103,26 +57,5 @@ end
 function IsMangaParkV3()
 
     return url:contains('/comic/')
-
-end
-
-function GetComicId()
-
-    return url:regex('\\/(?:title|comic)\\/(\\d+)', 1)
-
-end
-
-function GetApiUrl()
-
-    return '/apo/'
-
-end
-
-function GetApiJson(postDataStr)
-
-    http.Headers['accept'] = '*/*'
-    http.Headers['content-type'] = 'application/json'
-
-    return Json.New(http.Post(GetApiUrl(), postDataStr))
 
 end
