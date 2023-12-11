f544285b7f452b9e0ee38081f8ae73e742e89017
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2023-12-11 12:58:41 -0700

    hdporncomics.com: Added chapters detection

--- modules/lua/WpComicsPorno.lua
+++ modules/lua/WpComicsPorno.lua
@@ -13,12 +13,29 @@ function GetInfo()
     info.Title = CleanTitle(dom.SelectValue('//h1'))
     info.Artist = dom.SelectValues('//span[contains(text(), "Artist")]/following-sibling::span//a')
     info.Tags = dom.SelectValues('//span[contains(text(), "Tags")]/following-sibling::span//a')
+    info.Summary = dom.SelectValue('//div[contains(@id,"summary")]')
+
+end
+
+function GetChapters()
+
+    -- Note that not all galleries have chapters.
+
+    chapters.AddRange(dom.SelectElements('//div[contains(@id,"allChapters")]//a'))
+
+    chapters.Reverse()
 
 end
 
 function GetPages()
 
     pages.AddRange(dom.SelectValues('//div[contains(@class,"my-gallery")]//a/@href'))
+
+    -- Get images from the reader.
+
+    if(isempty(pages)) then
+        pages.AddRange(dom.SelectValues('//div[contains(@id,"imageContainer")]//img/@src'))
+    end
 
 end
 
