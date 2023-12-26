cf4e1ba0457e83e5e84ab559336dafe289f95bd2
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2023-12-26 02:29:57 -0700

    comick.fun: Moved to comick.cc

--- modules/lua/ComicK.lua
+++ modules/lua/ComicK.lua
@@ -2,86 +2,56 @@ function Register()
 
     module.Name = 'ComicK'
 
+    module.Domains.Add('comick.app')
+    module.Domains.Add('comick.cc')
     module.Domains.Add('comick.fun')
+    module.Domains.Add('comick.ink')
 
 end
 
 function GetInfo()
 
-    info.Title = dom.SelectValue('//h1')
-    info.AlternativeTitle = dom.SelectValue('//h1/following-sibling::div'):split(',')
-    info.Type = dom.SelectValue('//span[contains(text(),"Origination")]/following-sibling::span')
-    info.Tags = dom.SelectValues('//span[contains(text(),"Genres")]/following-sibling::span/a')
-    info.Artist = dom.SelectValues('//span[contains(text(),"Artists")]/following-sibling::span/a')
-    info.Author = dom.SelectValues('//span[contains(text(),"Authors")]/following-sibling::span/a')
-    info.DateReleased = dom.SelectValues('//span[contains(text(),"Published")]/following-sibling::span')
-    info.Status = dom.SelectValue('//span[contains(text(),"Status")]/following-sibling::span'):after('📖')
-    info.Summary = dom.SelectValue('//div[contains(@class,"comic-desc")]/text()[1]')
+    local json = GetApiJson('comic/' .. GetComicSlug())
 
-    local pageCount = GetImageUrls().Count()
-
-    if(pageCount > 0) then
-        info.PageCount = pageCount
-    end
+    info.Title = json.SelectValue('comic.title')
+    info.Status = json.SelectValue('comic.status') == '2' and 'Completed' or 'Ongoing'
+    info.Adult = toboolean(json.SelectValue('comic.hentai'))
+    info.Summary = json.SelectValue('comic.desc')
+    info.AlternativeTitle = json.SelectValues('comic.md_titles[*].title')
+    info.Tags = json.SelectValues('comic.md_comic_md_genres[*].md_genres.name')
+    info.Artist = json.SelectValues('artists[*].name')
+    info.Author = json.SelectValues('authors[*].name')
 
 end
 
 function GetChapters()
 
-    local baseUrl = url:before('?'):trim('/')..'/'
-    local comicId = tostring(dom):regex('{"id":(\\d+)', 1)
-    local apiEndpoint = '/api/get_chapters?comicid='..comicId..'&page='
-    local currentPage = 1
-    local totalChapters = 0
+    local json = GetApiJson('comic/' .. GetComicSlug())
+    local slug = json.SelectValue('comic.slug')
+    local hid = json.SelectValue('comic.hid')
 
-    repeat
+    json = GetApiJson('comic/' .. hid .. '/chapters')
 
-        local json = Json.New(http.Get(apiEndpoint..currentPage))
-        local chapterNodes = json.SelectTokens('data.chapters[*]')
+    for chapterNode in json.SelectNodes('chapters[*]') do
 
-        for chapterNode in chapterNodes do
+        local title = chapterNode.SelectValue('title')
+        local chapterNumber = chapterNode.SelectValue('chap')
+        local volumeNumber = chapterNode.SelectValue('vol')
+        local translator = chapterNode.SelectValues('group_name[*]')
+        local language = chapterNode.SelectValue('lang')
+        local hid = chapterNode.SelectValue('hid')
+        
+        local chapterInfo = ChapterInfo.New()
 
-            local id = chapterNode.SelectValue('id')
-            local number = chapterNode.SelectValue('chap')
-            local title = chapterNode.SelectValue('title')
-            local volume = chapterNode.SelectValue('vol')
-            local language = chapterNode.SelectValue('langName')
-            local iso6391 = chapterNode.SelectValue('iso639_1')
-            local groups = chapterNode.SelectValues('md_groups[*].title').Join(', ')
+        chapterInfo.Title = 'Ch. ' .. chapterNumber .. ' ' .. title
+        chapterInfo.Volume = volumeNumber
+        chapterInfo.Translator = translator
+        chapterInfo.Language = language
+        chapterInfo.Url = '/comic/' .. slug .. '/' .. hid .. '-chapter-' .. chapterNumber .. '-' .. language
 
-            local chapter = ChapterInfo.New()
+        chapters.Add(chapterInfo)
 
-            chapter.Url = baseUrl..id..'-chapter-'..number..'-'..iso6391
-            chapter.Translator = groups
-            chapter.Language = language
-
-            if(not isempty(number) and number ~= 'null') then
-                chapter.Title = chapter.Title..' Ch. '..number
-            end
-
-            if(not isempty(volume) and volume ~= 'null') then
-                chapter.Title = chapter.Title..' Vol. '..volume
-            end
-
-            if(not isempty(title) and title ~= 'null') then
-                chapter.Title = chapter.Title..' '..title
-            end
-
-            chapters.Add(chapter)
-
-        end
-
-        if(chapterNodes.Count() <= 0) then
-            break
-        end
-
-        if(totalChapters <= 0) then
-            totalChapters = tonumber(json.SelectValue('data.total'))
-        end
-
-        currentPage = currentPage + 1
-
-    until(chapters.Count() >= totalChapters)
+    end
 
     chapters.Reverse()
 
@@ -89,26 +59,53 @@ end
 
 function GetPages()
 
-    pages.AddRange(GetImageUrls())
+    local json = GetApiJson('chapter/' .. GetChapterHid())
+    local imagesPath = GetRoot(dom.SelectValue('//meta[contains(@property,"og:image")]/@content'))
 
-    -- googleusercontent URLs will 403 with a referer.
+    if(isempty(imagesPath)) then
+        imagesPath = '//meo.comick.pictures/'
+    end
 
-    pages.Referer = ''
+    for fileName in json.SelectValues('chapter.md_images[*].b2key') do
+    
+        local imageUrl = imagesPath .. fileName
+
+        pages.Add(imageUrl)
+
+    end
+    
+end
+
+function GetComicSlug()
+
+    return url:regex('\\/comic\\/([^\\/]+)', 1)
 
 end
 
-function GetImageUrls()
+function GetChapterHid()
 
-    local pages = List.New()
+    return url:regex('\\/comic\\/[^\\/]+\\/([^\\-]+)', 1)
 
-    for srcset in dom.SelectValues('//div[contains(@class,"images-reader")]//picture/img/@srcset') do
+end
 
-        local pageUrl = srcset:split(',').Last():trim():split(' ').First()
+function GetApiUrl()
 
-        pages.Add(pageUrl)
+    return '//api.' .. module.Domain .. '/'
 
-    end
+end
 
-    return pages
+function SetUpApiHeaders()
+
+    http.Headers['accept'] = '*/*'
+    http.Headers['origin'] = GetRoot(url):trim('/')
+    http.Headers['referer'] = GetRoot(url)
+
+end
+
+function GetApiJson(endpoint)
+
+    SetUpApiHeaders()
+
+    return Json.New(http.Get(GetApiUrl() .. endpoint))
 
 end
