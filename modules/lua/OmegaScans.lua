cd308d43696a070a9ffa53207639689bcb758520
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2024-02-06 18:29:01 -0700

    omegascans.org: Fixed chapter list

--- modules/lua/OmegaScans.lua
+++ modules/lua/OmegaScans.lua
@@ -1,59 +1,56 @@
 function Register()
 
-    module.Name = 'Omega Scans'
-    module.Language = 'English'
-    module.Adult = true
+    module.Name = 'Omega Scans'
+    module.Language = 'English'
+    module.Adult = true
 
-    module.Domains.Add('omegascans.org')
+    module.Domains.Add('omegascans.org')
 
 end
 
 function GetInfo()
 
-    info.Title = dom.SelectValue('//h1')
-    info.Summary = dom.SelectValue('//h5[contains(text(),"Description")]//following-sibling::div/p')
-    info.AlternativeTitle = dom.SelectValue('//h1//following-sibling::p')
-    info.DateReleased = dom.SelectValue('//p[contains(text(),"Release year")]/strong')
-    info.Author = dom.SelectValue('//p[contains(text(),"Author: ")]/strong')
-    info.Artist = dom.SelectValue('//p[contains(text(),"Author: ")]/strong')
-    info.Scanlator = module.Name
-    info.Publisher = dom.SelectValue('//p[contains(text(),"This is a series produced by")]/strong')
+    info.Title = dom.SelectValue('//h1')
+    info.Summary = dom.SelectValue('//h5[contains(text(),"Description")]//following-sibling::div/p')
+    info.AlternativeTitle = dom.SelectValue('//h1//following-sibling::p')
+    info.DateReleased = dom.SelectValue('//p[contains(text(),"Release year")]/strong')
+    info.Author = dom.SelectValue('//p[contains(text(),"Author: ")]/strong')
+    info.Artist = dom.SelectValue('//p[contains(text(),"Author: ")]/strong')
+    info.Scanlator = module.Name
+    info.Publisher = dom.SelectValue('//p[contains(text(),"This is a series produced by")]/strong')
 
-    local checkEndStatus = dom.SelectValue(
-        '//div[@id="radix-:R4hmmmeja:-content-1"]/ul//a[starts-with(@href, "/series/")][1]//li//span[contains(@class, "line-clamp-1" ) and contains(text(),"END") or contains(text(),"end")]')
+    local checkEndStatus = dom.SelectValue(
+        '//div[@id="radix-:R4hmmmeja:-content-1"]/ul//a[starts-with(@href, "/series/")][1]//li//span[contains(@class, "line-clamp-1" ) and contains(text(),"END") or contains(text(),"end")]')
 
-    if (not isempty(checkEndStatus)) then
-
-        info.Status = 'Completed'
-
-    end
+    if (not isempty(checkEndStatus)) then
+        info.Status = 'Completed'
+    end
 
 end
 
 function GetChapters()
 
-    for chapterNode in dom.SelectElements(
-        '//div[@id="radix-:R4hmmmeja:-content-1"]/ul//a[starts-with(@href, "/series/")]') do
+    for chapterNode in dom.SelectElements('//div[contains(@role,"tabpanel")]//a[contains(@href,"/series/")]') do
+        
+        local isPremiumChapter = chapterNode.SelectElements('.//span/*[name()="svg"]').Count() > 0
 
-        local premiumChapter = chapterNode.SelectElements('.//span/*[name()="svg"]')
+        if(not isPremiumChapter) then
 
-        if (premiumChapter.Count() == 0) then
+            local chapterUrl = chapterNode.SelectValue('@href')
+            local chapterTitle = chapterNode.SelectValue('.//li//span[contains(@class, "line-clamp-1")]')
+            
+            chapters.Add(chapterUrl, chapterTitle)
 
-            local chapterUrl = chapterNode.SelectValue('@href')
-            local chapterTitle = chapterNode.SelectValue('.//li//span[contains(@class, "line-clamp-1")]')
+        end
 
-            chapters.Add(chapterUrl, chapterTitle)
+    end
 
-        end
-
-    end
-
-    chapters.Reverse()
+    chapters.Reverse()
 
 end
 
 function GetPages()
 
-    pages.AddRange(dom.SelectValues('//img[contains(@data-src, "/uploads/series/")]/@data-src|//img[contains(@src, "/uploads/series/")]/@src'))
+    pages.AddRange(dom.SelectValues('//img[contains(@data-src, "/uploads/series/")]/@data-src|//img[contains(@src, "/uploads/series/")]/@src'))
 
 end
