f7b5de1429dd76c52bb4c923339ec3a7707eeb32
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2024-02-21 00:03:03 -0700

    olympusvisor.com: Moved to visorolym.com

--- modules/lua/OlympusScanlation.lua
+++ modules/lua/OlympusScanlation.lua
@@ -7,25 +7,33 @@ function Register()
     module.Domains.Add('olympusscans.com')
     module.Domains.Add('olympusv2.gg')
     module.Domains.Add('olympusvisor.com')
+    module.Domains.Add('visorolym.com')
 
 end
 
 function GetInfo()
 
-    info.Title = dom.SelectValue("//h1")
+    local apiEndpoint = GetApiUrl() .. GetSlug()
+    local json = Json.New(http.Get(apiEndpoint))
+
+    info.Title = json.SelectValue('data.name')
+    info.Summary = json.SelectValue('data.summary')
+
+    if(isempty(info.Title)) then
+        info.Title = dom.SelectValue("//h1") 
+    end
 
 end
 
 function GetChapters()
 
-    local slug = url:regex('\\/series\\/(?:comic-)?([^\\/]+)', 1)
     local seriesSlug = url:regex('\\/series\\/([^\\/]+)', 1)
     local currentPageIndex = 1
     local lastPageIndex = 1
 
     repeat
 
-        local apiEndpoint = '//dashboard.' .. module.Domain .. '/api/series/' .. slug .. '/chapters?page=' .. currentPageIndex .. '&direction=asc&type=comic'
+        local apiEndpoint = GetApiUrl() .. GetSlug() .. '/chapters?page=' .. currentPageIndex .. '&direction=asc&type=comic'
         local chaptersJson = Json.New(http.Get(apiEndpoint))
         local lastPageStr = chaptersJson.SelectValue('meta.last_page')
         local chapterNodes = chaptersJson.SelectTokens('data[*]')
@@ -60,4 +68,12 @@ function GetPages()
         pages.AddRange(dom.SelectValues('//img[@loading]/@src'))
     end
         
+end
+
+function GetApiUrl()
+    return '//dashboard.' .. module.Domain .. '/api/series/'
+end
+
+function GetSlug()
+    return url:regex('\\/series\\/(?:comic-)?([^\\/]+)', 1)    
 end
