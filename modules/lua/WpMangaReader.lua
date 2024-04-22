e1ef34cb59834c461036369473d1afe7fc1bb236
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2024-04-22 00:22:13 -0500

    Added support for tecnoscann.com

--- modules/lua/WpMangaReader.lua
+++ modules/lua/WpMangaReader.lua
@@ -66,6 +66,7 @@ function Register()
     module.Domains.Add('ragnascan.com', 'Ragna Scan')
     module.Domains.Add('raikiscan.com', 'Raiki Scan')
     module.Domains.Add('shotasekai.club', 'Shota Sekai')
+    module.Domains.Add('tecnoscann.com', 'Tecno scan')
 
     RegisterModule(module)
 
