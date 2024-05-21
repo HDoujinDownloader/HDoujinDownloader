30a1ff44c6bbe7c7920b5ced3963a986fc899c9e
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2024-05-21 12:44:56 -0500

    anchira.to: Updated API endpoint

--- modules/lua/Anchira.lua
+++ modules/lua/Anchira.lua
@@ -10,15 +10,18 @@ function Register()
 end
 
 local function GetApiUrl()
-    return '/api/v1/'
+    return '//api.anchira.to/'
 end
 
 local function GetApiJson(path)
    
-    path = GetApiUrl() .. path
+    if(not path:startswith('//') and not path:startswith('https://')) then
+        path = GetApiUrl() .. path
+    end
 
     http.Headers['Accept'] = '*/*'
-    http.Headers['Referer'] = url
+    http.Headers['Origin'] = 'https://' .. module.Domain
+    http.Headers['Referer'] = 'https://' .. module.Domain .. '/'
     http.Headers['X-Requested-With'] = 'XMLHttpRequest'
 
     local jsonStr = http.Get(path)
@@ -39,14 +42,48 @@ local function GetGalleryPath()
     return url:regex('\\/g\\/(.+?\\/.+?)$', 1)
 end
 
+local function IsGalleryUrl(url)
+    return url:contains('/g/')
+end
+
 function GetInfo()
 
-    local json = GetApiJson('library/' .. GetGalleryPath())
+    if(IsGalleryUrl(url)) then
 
-    info.Title = json.SelectValue('title')
-    info.PageCount = json.SelectValue('pages')
-    info.Tags = json.SelectValues('tags[*].name')
-    info.Artist = json.SelectValues('tags[?(@.namespace==1)].name')
+        local json = GetApiJson('library/' .. GetGalleryPath())
+
+        info.Title = json.SelectValue('title')
+        info.PageCount = json.SelectValue('pages')
+        info.Tags = json.SelectValues('tags[*].name')
+        info.Artist = json.SelectValues('tags[?(@.namespace==1)].name')
+        
+    else
+
+        -- Assume that we added a search/tag URL instead.
+        -- Add all galleries on the search page to the download queue.
+
+        local searchParameter = GetParameter(url, "s")
+
+        if(not isempty(searchParameter)) then
+
+            local endpoint = '/library?s=' .. searchParameter
+            local json = GetApiJson(endpoint)
+           
+            for entryNode in json.SelectNodes('entries[*]') do
+
+                local id = entryNode.SelectValue('id')
+                local key = entryNode.SelectValue('key')
+                local url = '/g/' .. id .. '/' .. key
+
+                Enqueue(url)
+
+            end
+
+            info.Ignore = true
+
+        end
+
+    end
 
 end
 
@@ -58,22 +95,29 @@ function GetPages()
 
     -- We get the image file names from the gallery metadata, and the path information from the library data.
 
-    local dataUrl = '//kisakisexo.xyz'
+    local dataUrls = { 'kisakisexo.xyz', 'aronasexo.xyz' }
 
     local galleryJson = GetApiJson('library/' .. GetGalleryPath())
     local libraryJson = GetApiJson('library/' .. GetGalleryPath() .. '/data')
-    
+
     local id = libraryJson.SelectValue('id')
     local key = libraryJson.SelectValue('key')
     local hash = libraryJson.SelectValue('hash')
     local server = toboolean(module.Settings['Data saver']) and 'b' or 'a'
+    local imageIndex = 0
 
     for name in galleryJson.SelectValues('data[*].n') do
         
-        local pageUrl = dataUrl .. '/' .. id .. '/' .. key .. '/' .. hash .. '/' .. server .. '/' .. EncodeUriComponent(name)
+        local pageUrl = '//' .. dataUrls[(imageIndex % #dataUrls) + 1] .. '/' .. id .. '/' .. key .. '/' .. hash .. '/' .. server .. '/' .. EncodeUriComponent(name)
 
         pages.Add(pageUrl)
 
+        imageIndex = imageIndex + 1
+
     end
+
+    pages.Headers['Accept'] = '*/*'
+    pages.Headers['Origin'] = 'https://' .. module.Domain
+    pages.Headers['Referer'] = 'https://' .. module.Domain .. '/'
 
 end
