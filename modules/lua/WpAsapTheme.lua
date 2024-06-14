61ced606105934c40411911099abcfcb2e87d2eb
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2024-06-14 16:51:32 -0600

    comics18.org: Fixed download

--- modules/lua/WpAsapTheme.lua
+++ modules/lua/WpAsapTheme.lua
@@ -1,3 +1,5 @@
+-- The "Orbital" theme is a near identical variant.
+
 function Register()
 
     module.Name = 'Asap Theme'
@@ -11,13 +13,21 @@ end
 
 function GetInfo()
 
-        info.Title = dom.SelectValue('//h1')
-        info.Tags = dom.SelectValues('//div[contains(@class,"content-tags")]//a')
+    info.Title = dom.SelectValue('//h1')
+    info.Tags = dom.SelectValues('//div[contains(@class,"content-tags")]//a')
+
+    if(isempty(info.Tags)) then
+        info.Tags = dom.SelectValues('//div[contains(@class,"tagcloud")]//a')
+    end
 
 end
 
 function GetPages()
 
     pages.AddRange(dom.SelectValues('//div[contains(@class,"the-content")]//img/@src'))
+
+    if(isempty(pages)) then -- Orbital
+        pages.AddRange(dom.SelectValues('//div[contains(@class,"entry-content")]//p/img/@src'))
+    end
 
 end
