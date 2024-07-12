459855981a706c865bd8b7aa03a14ec3e72859ee
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2024-07-12 12:25:26 -0600

    anchira.to: Moved to koharu.to

--- modules/lua/Anchira.lua
+++ modules/lua/Anchira.lua
@@ -3,18 +3,19 @@ function Register()
     module.Name = 'Anchira'
     module.Adult = true
 
-    module.Domains.Add('anchira.to')
+    module.Domains.Add('anchira.to', 'Anchira')
+    module.Domains.Add('koharu.to', 'Koharu')
 
     module.Settings.AddCheck('Data saver', false)
 
 end
 
 local function GetApiUrl()
-    return '//api.anchira.to/'
+    return '//api.' .. module.Domain .. '/'
 end
 
-local function GetApiJson(path)
-   
+local function GetApiJson(path, post)
+
     if(not path:startswith('//') and not path:startswith('https://')) then
         path = GetApiUrl() .. path
     end
@@ -22,9 +23,8 @@ local function GetApiJson(path)
     http.Headers['Accept'] = '*/*'
     http.Headers['Origin'] = 'https://' .. module.Domain
     http.Headers['Referer'] = 'https://' .. module.Domain .. '/'
-    http.Headers['X-Requested-With'] = 'XMLHttpRequest'
 
-    local jsonStr = http.Get(path)
+    local jsonStr = post and http.Post(path) or http.Get(path)
 
     if(not jsonStr:startswith('{')) then
         
@@ -39,23 +39,25 @@ local function GetApiJson(path)
 end
 
 local function GetGalleryPath()
-    return url:regex('\\/g\\/(.+?\\/.+?)$', 1)
+    return url:regex('\\/(?:g|reader)\\/([^\\/]+?\\/[^\\/#?]+)', 1)
 end
 
 local function IsGalleryUrl(url)
-    return url:contains('/g/')
+    return url:contains('/g/') or url:contains('/reader/')
 end
 
 function GetInfo()
 
     if(IsGalleryUrl(url)) then
 
-        local json = GetApiJson('library/' .. GetGalleryPath())
+        local json = GetApiJson('books/detail/' .. GetGalleryPath())
 
         info.Title = json.SelectValue('title')
-        info.PageCount = json.SelectValue('pages')
-        info.Tags = json.SelectValues('tags[*].name')
         info.Artist = json.SelectValues('tags[?(@.namespace==1)].name')
+        info.Language = json.SelectValues('tags[?(@.namespace==11)].name')
+        info.Magazine = json.SelectValues('tags[?(@.namespace==4)].name')
+        info.PageCount = json.SelectValues('thumbnails.entries[*]').Count()
+        info.Tags = json.SelectValues('tags[*].name')
         
     else
 
@@ -66,13 +68,13 @@ function GetInfo()
 
         if(not isempty(searchParameter)) then
 
-            local endpoint = '/library?s=' .. searchParameter
+            local endpoint = '/books?s=' .. searchParameter
             local json = GetApiJson(endpoint)
            
             for entryNode in json.SelectNodes('entries[*]') do
 
                 local id = entryNode.SelectValue('id')
-                local key = entryNode.SelectValue('key')
+                local key = entryNode.SelectValue('public_key')
                 local url = '/g/' .. id .. '/' .. key
 
                 Enqueue(url)
@@ -89,31 +91,19 @@ end
 
 function GetPages()
 
-    -- local appJsUrl = dom.SelectValue('//script[contains(@src,"/_app/")]/@src')
-    -- local appJs = http.Get(appJsUrl)
-    -- local dataUrl = appJs:regex('DATA_URL:\\s*\\"([^"]+)', 1)
+    local dataSaver = toboolean(module.Settings['Data saver'])
+    local galleryJson = GetApiJson('books/detail/' .. GetGalleryPath())
+   
+    local dataIndex = dataSaver and "1280" or "0"
+    local dataJson = galleryJson.SelectNode('data.' .. dataIndex)
+    local id = dataJson.SelectValue('id')
+    local key = dataJson.SelectValue('public_key')
 
-    -- We get the image file names from the gallery metadata, and the path information from the library data.
+    local readerJson = GetApiJson('books/data/' .. GetGalleryPath() .. '/' .. id .. '/' .. key, true)
+    local baseUrl = readerJson.SelectValue('base')
 
-    local dataUrls = { 'kisakisexo.xyz', 'aronasexo.xyz' }
-
-    local galleryJson = GetApiJson('library/' .. GetGalleryPath())
-    local libraryJson = GetApiJson('library/' .. GetGalleryPath() .. '/data')
-
-    local id = libraryJson.SelectValue('id')
-    local key = libraryJson.SelectValue('key')
-    local hash = libraryJson.SelectValue('hash')
-    local server = toboolean(module.Settings['Data saver']) and 'b' or 'a'
-    local imageIndex = 0
-
-    for name in galleryJson.SelectValues('data[*].n') do
-        
-        local pageUrl = '//' .. dataUrls[(imageIndex % #dataUrls) + 1] .. '/' .. id .. '/' .. key .. '/' .. hash .. '/' .. server .. '/' .. EncodeUriComponent(name)
-
-        pages.Add(pageUrl)
-
-        imageIndex = imageIndex + 1
-
+    for imageUrl in readerJson.SelectValues('entries[*].path') do
+        pages.Add(baseUrl .. imageUrl)
     end
 
     pages.Headers['Accept'] = '*/*'
