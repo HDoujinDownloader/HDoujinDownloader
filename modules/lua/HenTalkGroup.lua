e732c66eab9a0a9348625e1a371002c409b089d9
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2024-08-20 19:18:22 -0600

    fakku.cc: Moved to hentalk.pw (fixes #374)

--- modules/lua/HenTalkGroup.lua
+++ modules/lua/HenTalkGroup.lua
@@ -5,25 +5,65 @@ function Register()
     module.Adult = true
 
     module.Domains.Add('fakku.cc')
+    module.Domains.Add('hentalk.pw')
+    module.Domains.Add('spy.fakku.cc')
+
+end
+
+local function GetGalleryJson()
+
+    local galleryDataScript = dom.SelectValue('//script[contains(text(),"const data")]')
+        :regex('const\\s*data\\s*=\\s.+?;')
+
+    local js = JavaScript.New(galleryDataScript)
+    local galleryJson = js.Execute('data[2]').ToJson()
+
+    return galleryJson.SelectToken('..archive')
 
 end
 
 function GetInfo()
 
-    info.Title = dom.SelectValue('//h1')
-    info.Artist = dom.SelectValues('//tr[contains(@class,"artists")]//a')
-    info.Magazine = dom.SelectValues('//tr[contains(@class,"magazines")]//a')
+    local json = GetGalleryJson()
+
+    info.Title = json.SelectValue('title')
+    info.Summary = json.SelectValue('description')
+    info.Artist = json.SelectValues('artists[*].name')
+    info.Magazine = json.SelectValues('magazines[*].name')
+
+    local tags = List.New()
+
+    for tagNode in json.SelectNodes('tags[*]') do
+
+        local namespace = tagNode.SelectValue('namespace')
+        local name = tagNode.SelectValue('name')
+
+        if(not isempty(namespace)) then
+            tags.Add(namespace .. ':' .. name)
+        else
+            tags.Add(name)
+        end
+
+    end
+
+    info.Tags = tags
 
 end
 
 function GetPages()
 
-    pages.AddRange(dom.SelectValues('//div[contains(@class,"previews")]//a/@href'))
+    local json = GetGalleryJson()
+    local hash = json.SelectValue('hash')
 
-end
+    local cdnUrl = dom.SelectValue('//script[contains(text(),"PUBLIC_CDN_URL")]')
+        :regex('"PUBLIC_CDN_URL":"([^"]+)"', 1)
 
-function BeforeDownloadPage()
+        if(isempty(cdnUrl)) then
+            cdnUrl = '//cdn.fakku.cc'
+        end
 
-    page.Url = dom.SelectValue('//main//img/@src')
+    for fileName in json.SelectValues('images[*].filename') do
+        pages.Add(cdnUrl .. '/image/' .. hash .. '/' .. fileName)
+    end
 
 end
