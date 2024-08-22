055041c17fb98191d8bdb78713232408e2998d0c
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2024-08-22 03:44:02 -0600

    viewcartoon.com: Fix image ordering (fixes #378)

--- modules/lua/ViewCartoon.lua
+++ modules/lua/ViewCartoon.lua
@@ -7,41 +7,61 @@ function Register()
 
 end
 
+local function GetPdfDownloadUrl(dom)
+
+    local pdfDownloadUrl = dom.SelectValue('//script[contains(text(), "initPDFViewer")]')
+        :regex('initPDFViewer\\("([^"]+)"', 1)
+
+    if(not isempty(pdfDownloadUrl)) then
+        pdfDownloadUrl = '/manga/' .. pdfDownloadUrl .. '.jpg'
+    end
+
+    return pdfDownloadUrl
+
+end
+
+local function GetPdfPageInfo(dom)
+
+    local pageInfo = PageInfo.New(GetPdfDownloadUrl(dom))
+
+    pageInfo.ExtractContents = true
+    pageInfo.FileExtensionHint = '.pdf'
+
+    return pageInfo
+
+end
+
 function GetInfo()
 
-    info.Title = dom.SelectValue('//b')
-    info.Summary = dom.SelectValue('//div[b]/following-sibling::text()')
-    info.Tags = dom.SelectValue('//b[contains(text(),"tag:")]/following-sibling::text()')
+    info.Title = dom.SelectValue('//h1')
+    info.Summary = dom.SelectValue('//h1/following-sibling::p[string-length(text()) > 0]')
+    info.Tags = dom.SelectValue('//p[contains(.,"tag:")]/text()')
 
 end
 
 function GetChapters()
 
     chapters.AddRange(dom.SelectElements('//a[@target="vc"]'))
-
     chapters.Reverse()
 
 end
 
 function GetPages()
     
-    -- The images are delivered as a PDF (with the ".jpg" file extension?)
-
-    local pdfDownloadUrl = dom.SelectValue('//script[contains(text(), "initPDFViewer")]')
-        :regex('initPDFViewer\\("([^"]+)"', 1)
+    local pdfDownloadUrl = GetPdfDownloadUrl(dom)
 
     if(not isempty(pdfDownloadUrl)) then
 
-        pdfDownloadUrl = '/manga/' .. pdfDownloadUrl .. '.jpg'
+        -- The images are delivered as a PDF (with the ".jpg" file extension?).
+        -- Some PDF chapters are paginated (#260).
 
-        local pageInfo = PageInfo.New(pdfDownloadUrl)
-
-        pageInfo.ExtractContents = true
-        pageInfo.FileExtensionHint = '.pdf'
-
-        pages.Add(pageInfo)
+        for page in Paginator.New(http, dom, '//a[contains(text(),"part") or input[contains(@value,">>>>")]]/@href') do
+            pages.Add(GetPdfPageInfo(page))
+        end
 
     else
+
+        -- The images are delivered normally.
         
         for page in Paginator.New(http, dom, '//a[contains(text(),"หน้าถัดไป")]/@href') do
 
