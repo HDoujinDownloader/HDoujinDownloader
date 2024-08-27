ec1dc1074f429553aaf59628a28c9443b89093fa
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2024-08-27 00:28:00 -0600

    artuntamed.com: Add login support

--- modules/lua/ArtUntamed.lua
+++ modules/lua/ArtUntamed.lua
@@ -8,20 +8,14 @@ function Register()
 end
 
 local function GetImagesFromMediaPage()
-
     return dom.SelectValues('//div[contains(@class,"media-container")]//img/@src')
-
 end
 
 function GetInfo()
 
     info.Title = dom.SelectValue('//title'):beforelast('|')
     info.Artist = info.Title:regex('Media added by\\s([^|]+)', 1)
-    info.PageCount = GetImagesFromMediaPage().Count()
-
-    if(tonumber(info.PageCount) <= 0) then
-        info.PageCount = '?'
-    end
+    info.PageCount = '?'
 
 end
 
@@ -34,17 +28,16 @@ function GetPages()
     -- /index.php?media/
 
     for page in Paginator.New(http, dom, '//div[contains(@class,"pageNav")]//a[contains(@class,"next")]/@href') do
-    
+
         -- Add all media URLs if this is a thumbnail gallery.
 
         pages.AddRange(page.SelectValues('//a[contains(@class,"js-lbImage")]/@href'))
-    
+
         -- Add all media URLs if this is a tag gallery.
 
         pages.AddRange(page.SelectValues('//*[contains(@class,"contentRow-title")]//a/@href'))
 
     end
-
 
     if(isempty(pages)) then
         pages.Add(url)
@@ -62,6 +55,33 @@ function BeforeDownloadPage()
         return
     end
 
+    page.Referer = page.Url
     page.Url = GetImagesFromMediaPage()[0]
+
+end
+
+function Login()
+
+    if(isempty(http.Cookies)) then
+
+        local endpoint = '/index.php?login/login'
+        local dom = Dom.New(http.Get(endpoint))
+        local xsrfToken = dom.SelectValue('//input[contains(@name,"_xfToken")]/@value')
+
+        http.PostData.Add('_xfToken', xsrfToken)
+        http.PostData.Add('login', username)
+        http.PostData.Add('password', password)
+        http.PostData.Add('remember', '1')
+        http.PostData.Add('_xfRedirect', '')
+
+        local response = http.PostResponse(endpoint)
+
+        if(not response.Cookies.Contains('xf_user')) then
+            Fail(Error.LoginFailed)
+        end
+
+        global.SetCookies(response.Cookies)
+
+    end
 
 end
