ac9de083c22832b30065c79514e9771c3ed792e8
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2024-09-18 14:35:40 -0600

    hentaivn.la: Moved to hentaiayame.net

--- modules/lua/HentaiVn.lua
+++ modules/lua/HentaiVn.lua
@@ -4,6 +4,7 @@ function Register()
     module.Language = 'Vietnamese'
     module.Adult = true
 
+    module.Domains.Add('hentaiayame.net')
     module.Domains.Add('hentaivn.la')
     module.Domains.Add('hentaivn.moe')
     module.Domains.Add('hentaivn.net')
