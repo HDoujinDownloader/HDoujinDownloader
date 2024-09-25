98aea524a038fd2cd3567d202595d9190c83b2bb
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2024-09-24 23:30:03 -0600

    hentairead.com: Fix metadata

--- modules/lua/HentaiRead.lua
+++ modules/lua/HentaiRead.lua
@@ -23,14 +23,15 @@ function GetInfo()
     if(url:contains('/hentai/')) then
 
         info.Title = dom.SelectValue('//h1')
-        info.OriginalTitle = dom.SelectValue('//h4')
-        info.Language = dom.SelectValues('//span[contains(text(),"Language")]/following-sibling::span//a/span[contains(@class,"name")]')
-        info.Type = dom.SelectValues('//span[contains(text(),"Category")]/following-sibling::span//a/span[contains(@class,"name")]')
-        info.Circle = dom.SelectValues('//span[contains(text(),"Circle")]/following-sibling::span//a/span[contains(@class,"name")]')
-        info.Parody = dom.SelectValues('//span[contains(text(),"Parody")]/following-sibling::span//a/span[contains(@class,"name")]')
-        info.Tags = dom.SelectValues('//span[contains(text(),"Tags")]/following-sibling::span//a/span[contains(@class,"name")]')
-        info.Scanlator = dom.SelectValues('//span[contains(text(),"Scanlator")]/following-sibling::span//a/span[contains(@class,"name")]')
-        info.DateReleased = dom.SelectValues('//span[contains(text(),"Release Year")]/following-sibling::span//a/span[contains(@class,"name")]')
+        info.OriginalTitle = dom.SelectValue('//h2')
+        info.Artist = dom.SelectValues('//div[contains(text(),"Artist")]/following-sibling::a/span[1]')
+        info.Language = dom.SelectValues('//div[contains(text(),"Language")]/following-sibling::a/span[1]')
+        info.Type = dom.SelectValues('//div[contains(text(),"Category")]/following-sibling::a/span[1]')
+        info.Circle = dom.SelectValues('//div[contains(text(),"Circle")]/following-sibling::a/span[1]')
+        info.Parody = dom.SelectValues('//div[contains(text(),"Parody")]/following-sibling::a/span[1]')
+        info.Tags = dom.SelectValues('//div[contains(text(),"Tags")]/following-sibling::a/span[1]')
+        info.Scanlator = dom.SelectValues('//div[contains(text(),"Scanlator")]/following-sibling::a/span[1]')
+        info.DateReleased = dom.SelectValues('//div[contains(text(),"Release Year")]/following-sibling::a/span[1]')
 
     else
 
