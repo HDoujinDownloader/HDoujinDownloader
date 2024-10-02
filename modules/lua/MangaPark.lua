6b71056f6d91b0334b7b0001bcfbbdf3293bbc8f
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2024-10-02 00:42:25 -0600

    Add support for mangapark.com

--- modules/lua/MangaPark.lua
+++ modules/lua/MangaPark.lua
@@ -15,6 +15,7 @@ function Register()
     module.Name = 'MangaPark'
     module.Language = 'English'
 
+    module.Domains.Add('mangapark.com')
     module.Domains.Add('mangapark.net')
     module.Domains.Add('mangapark.org')
 
@@ -25,9 +26,7 @@ function Register()
 end
 
 local function IsMangaParkV3()
-
     return url:contains('/comic/')
-
 end
 
 function GetInfo()
