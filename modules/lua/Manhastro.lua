3fb7491aaa5917493258e18c515e573d6dbbade8
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2025-01-10 01:02:00 -0600

    manhastro.com: Fix incomplete chapter list

--- modules/lua/Manhastro.lua
+++ modules/lua/Manhastro.lua
@@ -1,11 +1,43 @@
 require "Madara"
 
+local BaseGetChapters = GetChapters
+
 function Register()
 
     module.Name = 'Manhastro'
     module.Language = 'pt-br'
 
     module.Domains.Add('manhastro.com')
+
+end
+
+function GetChapters()
+
+    -- Only a partial list is accessible from the summary page.
+    -- We need to access one of the chapters to get a full chapter list.
+
+    local latestChapterUrl = dom.SelectValue('//div[contains(@class,"listing-chapters")]//a/@href')
+
+    if(isempty(latestChapterUrl)) then
+
+        BaseGetChapters()
+
+    else
+
+        dom = Dom.New(http.Get(latestChapterUrl))
+
+        for optionNode in dom.SelectElements('(//select[contains(@class,"single-chapter-select")])[1]//option') do
+
+            local chapterUrl = optionNode.SelectValue('./@data-redirect')
+            local chapterTitle = tostring(optionNode)
+
+            chapters.Add(chapterUrl, chapterTitle)
+
+        end
+
+        chapters.Reverse()
+
+    end
 
 end
 
