d2daaa3e3a8aa4394b0b759191a08241cf0a58f7
HDoujinDownloader <46956787+HDoujinDownloader@users.noreply.github.com>
2025-06-25 02:05:58 -0600

    hentaifox.com: Fall back to default image server

--- modules/lua/HentaiFox.lua
+++ modules/lua/HentaiFox.lua
@@ -1,21 +1,17 @@
 function Register()
-
     module.Name = "HentaiFox"
     module.Adult = true
 
-    module.Domains.Add('hentaifox.com')
-
+    module.Domains:Add('hentaifox.com')
 end
 
 
-local function GetPageCount()
+local function getPageCount()
     return tonumber(dom.SelectValue('//span[contains(@class, "pages")]'):after(':'))
 end
 
 function GetInfo()
-
-    if(url:contains('/gallery/')) then
-
+    if url:contains('/gallery/') then
         info.Title = dom.SelectValue('//h1')
         info.Parody = dom.SelectValues('//ul[@class="parodies"]//a/text()[1]')
         info.Characters = dom.SelectValues('//ul[@class="characters"]//a/text()[1]')
@@ -24,26 +20,19 @@ function GetInfo()
         info.Circle = dom.SelectValues('//ul[@class="groups"]//a/text()[1]')
         info.Language = dom.SelectValues('//ul[@class="languages"]//a/text()[1]')
         info.Type = dom.SelectValues('//ul[@class="categories"]//a/text()[1]')
-        info.PageCount = GetPageCount()
-
+        info.PageCount = getPageCount()
     else
-
         info.Title = dom.SelectValue('//h1'):beforelast('-')
 
         for galleryUrl in dom.SelectValues('//h2[contains(@class,"g_title")]/a/@href') do
-
             Enqueue(galleryUrl)
-
         end
 
         info.Ignore = true
-
     end
-
 end
 
 function GetPages()
-
     -- Make sure that the URL points to the reader.
 
     local readOnlineUrl = dom.SelectValue('//a[contains(@class, "g_button")]/@href')
@@ -58,19 +47,18 @@ function GetPages()
 
     local imageDir = dom.SelectValue('//input[@name="image_dir"]/@value')
     local galleryId = dom.SelectValue('//input[@name="gallery_id"]/@value')
-    local uniqueId = dom.SelectValue('//input[@name="unique_id"]/@value')
+    local uniqueId = tonumber(dom.SelectValue('//input[@name="unique_id"]/@value'))
 
     local thumbnailsJsonStr = tostring(dom):regex("var\\s*g_th\\s*=\\s*\\$\\.parseJSON\\('(.+?)'\\);", 1)
     local thumbnailsJson = Json.New(thumbnailsJsonStr)
 
     for key in thumbnailsJson.Keys do
-
         local pageNumber = key
         local pageExtension = tostring(thumbnailsJson[key]):split(',').First()
         local imageServers = { "i", "i2" }
         local imageServer = imageServers[math.random(1, #imageServers)]
 
-        if(tonumber(uniqueId) > 140236) then
+        if not uniqueId or uniqueId > 140236 then
             imageServer = "i3"
         end
 
@@ -86,7 +74,5 @@ function GetPages()
 
         pages.Add(FormatString('//{0}.{1}/{2}/{3}/{4}{5}',
             imageServer, module.Domain, imageDir, galleryId, pageNumber, pageExtension))
-
     end
-
 end
