b65d5c020c8996b0e85bd13b14169f09cf574d95
MarcVCE <43180462+MarcVCE@users.noreply.github.com>
2026-05-26 01:33:11 +0200

    Add new domains for Madara

--- modules/lua/Madara.lua
+++ modules/lua/Madara.lua
@@ -92,6 +92,8 @@ function Register()
     module.Domains.Add('seriesemperor.com', 'EmperorScan')
     module.Domains.Add('zonaemperor.com', 'EmperorScan')
     module.Domains.Add('submanhwa.net', 'SubManhwa')
+    module.Domains.Add('topcomicporno.com', 'TopComicPorno') -- Requires FlareSolverr + Chrome
+    module.Domains.Add('topcomicporno.net', 'TopComicPorno.net') -- Requires FlareSolverr + Chrome
 
     RegisterModule(module)
 
